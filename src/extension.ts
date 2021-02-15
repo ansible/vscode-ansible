a7ad8c4f61fba559c95f6bf0ca959f8afda5dced
Sorin Sbarnea <ssbarnea@redhat.com>
2021-02-15 15:25:21 +0000

    Remove hello world code

--- src/extension.ts
+++ src/extension.ts
@@ -11,21 +11,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-ansible" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-ansible.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-ansible!');
-	});
-
-
 	let validator = new AnsibleValidationProvider(context.workspaceState);
 	validator.activate(context.subscriptions);
 
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
