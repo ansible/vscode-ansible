13a5ab67d9ebab353da49c83e8e1f97ad77ad1b2
Tomasz Maciążek <tomaciazek@gmail.com>
2021-02-27 18:30:23 +0100

    Translated into language client/server duo

---
+++ client/src/extension.ts
@@ -0,0 +1,69 @@
+import * as path from 'path';
+import { workspace, ExtensionContext, languages } from 'vscode';
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from 'vscode-languageclient/node';
+import { DocumentSemanticTokensProvider, legend } from './semanticTokens';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext): void {
+  // The server is implemented in node
+  const serverModule = context.asAbsolutePath(
+    path.join('server', 'out', 'server.js')
+  );
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for Ansible documents
+    documentSelector: [{ scheme: 'file', language: 'ansible' }],
+    synchronize: {
+      // Notify the server about file changes to ansible.cfg files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher('**/ansible.cfg'),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    'ansibleServer',
+    'Ansible Server',
+    serverOptions,
+    clientOptions
+  );
+
+  context.subscriptions.push(
+    languages.registerDocumentSemanticTokensProvider(
+      { language: 'ansible' },
+      new DocumentSemanticTokensProvider(),
+      legend
+    )
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
---
+++ server/src/server.ts
@@ -0,0 +1,225 @@
+import IntervalTree from '@flatten-js/interval-tree';
+import * as _ from 'lodash';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import {
+  createConnection,
+  Diagnostic,
+  DiagnosticRelatedInformation,
+  DiagnosticSeverity,
+  DidChangeConfigurationNotification,
+  InitializeParams,
+  InitializeResult,
+  Location,
+  ProposedFeatures,
+  Range,
+  TextDocuments,
+  TextDocumentSyncKind,
+} from 'vscode-languageserver/node';
+import { parseAllDocuments } from 'yaml';
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+const connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+  const capabilities = params.capabilities;
+
+  // Does the client support the `workspace/configuration` request?
+  // If not, we fall back using global settings.
+  hasConfigurationCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.configuration
+  );
+  hasWorkspaceFolderCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.workspaceFolders
+  );
+  hasDiagnosticRelatedInformationCapability = !!(
+    capabilities.textDocument &&
+    capabilities.textDocument.publishDiagnostics &&
+    capabilities.textDocument.publishDiagnostics.relatedInformation
+  );
+
+  const result: InitializeResult = {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+      // Tell the client that this server supports code completion.
+      completionProvider: {
+        resolveProvider: true,
+      },
+    },
+  };
+  if (hasWorkspaceFolderCapability) {
+    result.capabilities.workspace = {
+      workspaceFolders: {
+        supported: true,
+      },
+    };
+  }
+  return result;
+});
+
+connection.onInitialized(() => {
+  if (hasConfigurationCapability) {
+    // Register for all configuration changes.
+    connection.client.register(
+      DidChangeConfigurationNotification.type,
+      undefined
+    );
+  }
+  if (hasWorkspaceFolderCapability) {
+    connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+      connection.console.log('Workspace folder change event received.');
+    });
+  }
+  connection.client;
+});
+
+// The example settings
+interface ExampleSettings {
+  maxNumberOfProblems: number;
+}
+
+// The global settings, used when the `workspace/configuration` request is not supported by the client.
+// Please note that this is not the case when using this server with the client provided in this example
+// but could happen with other clients.
+const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
+let globalSettings: ExampleSettings = defaultSettings;
+
+// Cache the settings of all open documents
+const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+
+connection.onDidChangeConfiguration((change) => {
+  if (hasConfigurationCapability) {
+    // Reset all cached document settings
+    documentSettings.clear();
+  } else {
+    globalSettings = <ExampleSettings>(
+      (change.settings.languageServerExample || defaultSettings)
+    );
+  }
+
+  // Revalidate all open text documents
+  documents.all().forEach(validateTextDocument);
+});
+
+function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
+  if (!hasConfigurationCapability) {
+    return Promise.resolve(globalSettings);
+  }
+  let result = documentSettings.get(resource);
+  if (!result) {
+    result = connection.workspace.getConfiguration({
+      scopeUri: resource,
+      section: 'languageServerExample',
+    });
+    documentSettings.set(resource, result);
+  }
+  return result;
+}
+
+// Only keep settings for open documents
+documents.onDidClose((e) => {
+  documentSettings.delete(e.document.uri);
+});
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent((change) => {
+  validateTextDocument(change.document);
+});
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+  // In this simple example we get the settings for every validate run.
+  const settings = await getDocumentSettings(textDocument.uri);
+
+  const diagnostics: Diagnostic[] = [];
+  const yDocuments = parseAllDocuments(textDocument.getText(), {
+    prettyErrors: false,
+  });
+  const rangeTree = new IntervalTree<Diagnostic>();
+  yDocuments.forEach((yDoc) => {
+    yDoc.errors.forEach((error) => {
+      const errorRange = error.range || error.source?.range;
+      let range;
+      if (errorRange) {
+        const start = textDocument.positionAt(errorRange.start);
+        const end = textDocument.positionAt(errorRange.end);
+        range = Range.create(start, end);
+
+        let severity;
+        switch (error.name) {
+          case 'YAMLReferenceError':
+          case 'YAMLSemanticError':
+          case 'YAMLSyntaxError':
+            severity = DiagnosticSeverity.Error;
+            break;
+          case 'YAMLWarning':
+            severity = DiagnosticSeverity.Warning;
+            break;
+          default:
+            severity = DiagnosticSeverity.Information;
+            break;
+        }
+        rangeTree.insert([errorRange.start, errorRange.end], {
+          message: error.message,
+          range: range || Range.create(0, 0, 0, 0),
+          severity: severity,
+          source: 'Ansible [YAML]',
+        });
+      }
+    });
+  });
+  rangeTree.forEach((range, diag) => {
+    const searchResult = rangeTree.search(range);
+    if (searchResult) {
+      const allRangesAreEqual = searchResult.every((foundDiag: Diagnostic) => {
+        // (range start == range end) in case it has already been collapsed
+        return (
+          foundDiag.range.start === foundDiag.range.end ||
+          _.isEqual(foundDiag.range, diag.range)
+        );
+      });
+      if (!allRangesAreEqual) {
+        // Prevent large error scopes hiding/obscuring other error scopes
+        // In YAML this is very common in case of syntax errors
+        const range = diag.range;
+        diag.relatedInformation = [
+          DiagnosticRelatedInformation.create(
+            Location.create(textDocument.uri, {
+              start: range.end,
+              end: range.end,
+            }),
+            'the scope of this error ends here'
+          ),
+        ];
+        // collapse the range
+        diag.range = {
+          start: range.start,
+          end: range.start,
+        };
+      }
+    }
+    diagnostics.push(diag);
+  });
+
+  // Send the computed diagnostics to VSCode.
+  connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
+
+connection.onDidChangeWatchedFiles((_change) => {
+  // Monitored files have change in VSCode
+  connection.console.log('We received a file change event');
+});
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
