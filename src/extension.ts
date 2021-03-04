6732e8474516a0457622606684e364a18ecae09b
Sorin Sbarnea <ssbarnea@redhat.com>
2021-03-04 18:57:32 +0000

    feat: use dedicated output channel (#41)
    Fixes: #35

--- src/extension.ts
+++ src/extension.ts
@@ -7,13 +7,13 @@ import AnsibleValidationProvider from './features/validationProvider';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-ansible" is now active!');
+	let output = vscode.window.createOutputChannel("Ansible");
 
 	let validator = new AnsibleValidationProvider(context.workspaceState);
 	validator.activate(context.subscriptions);
 
+	output.appendLine('Ansible Language extension is now active');
+	// output.show();
 }
 
 // this method is called when your extension is deactivated
