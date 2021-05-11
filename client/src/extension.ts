9512bfb97893cf5c0af1891e7f6ea8bbd24e760f
Tomasz Maciążek <tomaciazek@gmail.com>
2021-05-11 22:18:15 +0200

    Implemented multiroot workspace launching

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,28 +1,32 @@
 import * as path from 'path';
-import { workspace, ExtensionContext, languages } from 'vscode';
-
+import {
+  ExtensionContext,
+  languages,
+  OutputChannel,
+  TextDocument,
+  window as Window,
+  workspace as Workspace,
+  WorkspaceFolder,
+} from 'vscode';
 import {
   LanguageClient,
   LanguageClientOptions,
-  ServerOptions,
   TransportKind,
 } from 'vscode-languageclient/node';
 import { DocumentSemanticTokensProvider, legend } from './semanticTokens';
+import { WorkspaceManager } from './workspaceManager';
 
-let client: LanguageClient;
+let defaultClient: LanguageClient;
+const clients: Map<string, LanguageClient> = new Map();
 
-export function activate(context: ExtensionContext): void {
-  // The server is implemented in node
-  const serverModule = context.asAbsolutePath(
-    path.join('server', 'out', 'server.js')
-  );
-  // The debug options for the server
-  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-  const serverOptions: ServerOptions = {
+function getLanguageClient(
+  serverModule: string,
+  outputChannel: OutputChannel,
+  debugPort: number,
+  folder?: WorkspaceFolder
+): LanguageClient {
+  const debugOptions = { execArgv: ['--nolazy', `--inspect=${debugPort}`] };
+  const serverOptions = {
     run: { module: serverModule, transport: TransportKind.ipc },
     debug: {
       module: serverModule,
@@ -30,24 +34,100 @@ export function activate(context: ExtensionContext): void {
       options: debugOptions,
     },
   };
-
-  // Options to control the language client
-  const clientOptions: LanguageClientOptions = {
-    // Register the server for Ansible documents
-    documentSelector: [{ scheme: 'file', language: 'ansible' }],
-    synchronize: {
-      // Notify the server about file changes to ansible.cfg files contained in the workspace
-      fileEvents: workspace.createFileSystemWatcher('**/ansible.cfg'),
-    },
-  };
-
-  // Create the language client and start the client.
-  client = new LanguageClient(
+  const clientOptions: LanguageClientOptions = folder
+    ? {
+        documentSelector: [
+          {
+            scheme: 'file',
+            language: 'ansible',
+            pattern: `${folder.uri.fsPath}/**/*`,
+          },
+        ],
+        workspaceFolder: folder,
+        synchronize: {
+          // Notify the server about file changes to ansible.cfg files contained in the root folder
+          fileEvents: Workspace.createFileSystemWatcher(
+            `${folder.uri.fsPath}/**/ansible.cfg`
+          ),
+        },
+      }
+    : {
+        documentSelector: [{ scheme: 'untitled', language: 'ansible' }],
+        synchronize: {
+          // Notify the server about file changes to the top ansible.cfg file of the workspace
+          fileEvents: Workspace.createFileSystemWatcher('ansible.cfg'),
+        },
+      };
+  clientOptions.diagnosticCollectionName = 'ansible-lang';
+  clientOptions.outputChannel = outputChannel;
+  return new LanguageClient(
     'ansibleServer',
     'Ansible Server',
     serverOptions,
     clientOptions
   );
+}
+
+export function activate(context: ExtensionContext): void {
+  // The server is implemented in node
+  const serverModule = context.asAbsolutePath(
+    path.join('server', 'out', 'server.js')
+  );
+  const outputChannel: OutputChannel = Window.createOutputChannel(
+    'Ansible-Language-Server'
+  );
+
+  const workspaceManager = new WorkspaceManager();
+
+  function textDocumentOpenedHandler(document: TextDocument): void {
+    if (
+      // Check if we're dealing with a regular 'ansible' file
+      document.languageId === 'ansible' &&
+      (document.uri.scheme === 'file' || document.uri.scheme === 'untitled')
+    ) {
+      const uri = document.uri;
+      // Untitled files go to a default client.
+      if (uri.scheme === 'untitled' && !defaultClient) {
+        defaultClient = getLanguageClient(serverModule, outputChannel, 6009);
+        defaultClient.start();
+        return;
+      }
+      let folder = Workspace.getWorkspaceFolder(uri);
+      // Files outside a folder should be handled by the default client as well.
+      if (!folder && !defaultClient) {
+        defaultClient = getLanguageClient(serverModule, outputChannel, 6009);
+        defaultClient.start();
+        return;
+      }
+      if (folder) {
+        // If we have nested workspace folders we only start a server on the outer most workspace folder.
+        folder = workspaceManager.getOuterMostWorkspaceFolder(folder);
+
+        if (!clients.has(folder.uri.toString())) {
+          const client = getLanguageClient(
+            serverModule,
+            outputChannel,
+            6010 + clients.size,
+            folder
+          );
+          client.start();
+          clients.set(folder.uri.toString(), client);
+        }
+      }
+    }
+  }
+
+  Workspace.onDidOpenTextDocument(textDocumentOpenedHandler);
+  Workspace.textDocuments.forEach(textDocumentOpenedHandler);
+  Workspace.onDidChangeWorkspaceFolders((event) => {
+    for (const folder of event.removed) {
+      const client = clients.get(folder.uri.toString());
+      if (client) {
+        clients.delete(folder.uri.toString());
+        client.stop();
+      }
+    }
+  });
 
   context.subscriptions.push(
     languages.registerDocumentSemanticTokensProvider(
@@ -56,14 +136,15 @@ export function activate(context: ExtensionContext): void {
       legend
     )
   );
-
-  // Start the client. This will also launch the server
-  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-  if (!client) {
-    return undefined;
+  const promises: Thenable<void>[] = [];
+  if (defaultClient) {
+    promises.push(defaultClient.stop());
   }
-  return client.stop();
+  for (const client of clients.values()) {
+    promises.push(client.stop());
+  }
+  return Promise.all(promises).then(() => undefined);
 }
