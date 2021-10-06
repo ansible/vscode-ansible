75cad5ec321f3d7aa8f8fc810fae6cfb5af4162a
Ondrej Dockal <odockal@redhat.com>
2021-10-06 16:38:12 +0200

    feat: introduce ui tests based on vscode-extension-tester (#208)
    Co-authored-by: Sorin Sbarnea <ssbarnea@redhat.com>
    Co-authored-by: Sorin Sbarnea <sorin.sbarnea@gmail.com>
    Co-authored-by: Sviatoslav Sydorenko <webknjaz@redhat.com>

---
+++ src/ui-test/allTestsSuite.ts
@@ -0,0 +1,8 @@
+import { extensionUIAssetsTest } from './extensionUITest';
+
+/**
+ * @author Ondrej Dockal <odockal@redhat.com>
+ */
+describe('VSCode Ansible - UI tests', () => {
+  extensionUIAssetsTest();
+});
---
+++ src/ui-test/extensionUITest.ts
@@ -0,0 +1,52 @@
+import { expect } from 'chai';
+import {
+  ActivityBar,
+  SideBarView,
+  ViewControl,
+  ExtensionsViewSection,
+} from 'vscode-extension-tester';
+
+/**
+ * @author Ondrej Dockal <odockal@redhat.com>
+ */
+export function extensionUIAssetsTest(): void {
+  describe('Verify base assets are available after installation', () => {
+    let view: ViewControl;
+    let sideBar: SideBarView;
+
+    before(async function () {
+      this.timeout(4000);
+      view = (await new ActivityBar().getViewControl(
+        'Extensions'
+      )) as ViewControl;
+      sideBar = await view.openView();
+    });
+
+    it('VSCode Ansible extension is installed', async function () {
+      this.timeout(8000);
+      const section = (await sideBar
+        .getContent()
+        .getSection('Installed')) as ExtensionsViewSection;
+      const item = await section.findItem('@installed Ansible');
+      expect(item).not.undefined;
+      expect(await item?.getText()).to.contain('Red Hat');
+    });
+
+    after(async function () {
+      this.timeout(4000);
+      if (sideBar && (await sideBar.isDisplayed())) {
+        const viewControl = (await new ActivityBar().getViewControl(
+          'Extensions'
+        )) as ViewControl;
+        sideBar = await viewControl.openView();
+        const titlePart = sideBar.getTitlePart();
+        const actionButton = await titlePart.getAction(
+          'Clear Extensions Search Results'
+        );
+        if (await actionButton.isEnabled()) {
+          await actionButton.click();
+        }
+      }
+    });
+  });
+}
