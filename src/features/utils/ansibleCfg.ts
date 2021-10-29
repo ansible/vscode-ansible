54a37d0ad2b4f0eab83754c969bcdf9a45c4fb78
Julian Einwag <jeinwag@users.noreply.github.com>
2021-10-29 12:22:16 +0200

    Restore client-side support for working with ansible vaults (#177)
    Co-authored-by: Sviatoslav Sydorenko <webknjaz@redhat.com>
    Co-authored-by: Sorin Sbarnea <ssbarnea@redhat.com>

---
+++ src/features/utils/ansibleCfg.ts
@@ -0,0 +1,104 @@
+/* node "stdlib" */
+import * as fs from 'fs';
+
+/* vscode"stdlib" */
+import * as vscode from 'vscode';
+
+/* third-party */
+import untildify from 'untildify';
+import * as ini from 'ini';
+
+// Get rootPath based on multi-workspace API
+export function getRootPath(editorDocumentUri: vscode.Uri): string | undefined {
+  if (typeof vscode.workspace.getWorkspaceFolder !== 'function') {
+    return vscode.workspace.workspaceFolders?.[0]?.name;
+  }
+
+  return vscode.workspace.getWorkspaceFolder(editorDocumentUri)?.uri?.path;
+}
+
+export type AnsibleVaultConfig = {
+  path: string;
+  defaults: {
+    vault_identity_list: string | undefined;
+    vault_password_file: string | undefined;
+  };
+};
+
+export async function scanAnsibleCfg(
+  rootPath: string | undefined = undefined
+): Promise<AnsibleVaultConfig | undefined> {
+  /*
+   * Reading order:
+   * 1) ANSIBLE_CONFIG
+   * 2) ansible.cfg (in current workspace)
+   * 3) ~/.ansible.cfg
+   * 4) /etc/ansible.cfg
+   */
+  const cfgFiles = ['~/.ansible.cfg', '/etc/ansible.cfg'];
+
+  if (!!rootPath) {
+    cfgFiles.unshift(`${rootPath}/ansible.cfg`);
+  }
+
+  if (!!process.env.ANSIBLE_CONFIG) {
+    cfgFiles.unshift(process.env.ANSIBLE_CONFIG);
+  }
+
+  const cfgs = await Promise.all(
+    cfgFiles
+      .map((cf) => untildify(cf))
+      .map(async (cp) => await getValueByCfg(cp))
+  )?.catch(() => undefined);
+  const cfg = cfgs?.find(
+    (c) =>
+      !!c?.defaults?.vault_identity_list || !!c?.defaults?.vault_password_file
+  );
+  console.log(
+    typeof cfg != 'undefined'
+      ? `Found 'defaults.vault_identity_list' within '${cfg.path}'`
+      : 'Found no \'defaults.vault_identity_list\' within config files'
+  );
+
+  return cfg;
+}
+
+export async function getValueByCfg(
+  path: string
+): Promise<AnsibleVaultConfig | undefined> {
+  console.log(`Reading '${path}'...`);
+
+  try {
+    await fs.promises.access(path, fs.constants.R_OK);
+  } catch {
+    return undefined;
+  }
+
+  const parsedConfig = ini.parse(await fs.promises.readFile(path, 'utf-8'));
+  const vault_identity_list = parsedConfig?.defaults?.vault_identity_list;
+  const vault_password_file = parsedConfig?.defaults?.vault_password_file;
+
+  if (!vault_identity_list && !vault_password_file) {
+    return undefined;
+  }
+
+  return {
+    path: path,
+    defaults: { vault_identity_list, vault_password_file },
+  } as AnsibleVaultConfig;
+}
+
+export async function getAnsibleCfg(
+  path: string | undefined
+): Promise<AnsibleVaultConfig | undefined> {
+  if (!!process.env.ANSIBLE_VAULT_IDENTITY_LIST) {
+    return {
+      path: 'ANSIBLE_VAULT_IDENTITY_LIST',
+      defaults: {
+        vault_identity_list: process.env.ANSIBLE_VAULT_IDENTITY_LIST,
+        vault_password_file: undefined,
+      },
+    };
+  }
+  return scanAnsibleCfg(path);
+}
