d4ea3cd3f4e91465097d4140ed47079cfbf68d35
Ganesh Nalawade <ganesh634@gmail.com>
2021-11-11 09:50:50 +0530

    changed default execution environment image (#114)
    *  Update default execution environment image
       from `quay.io/ansible/ansible-navigator-demo-ee:0.6.0`
       to `quay.io/ansible/ansible-devtools-demo-ee:v0.1.0`

--- src/services/settingsManager.ts
+++ src/services/settingsManager.ts
@@ -19,7 +19,7 @@ export class SettingsManager {
     executionEnvironment: {
       containerEngine: 'auto',
       enabled: false,
-      image: 'quay.io/ansible/ansible-navigator-demo-ee:0.6.0',
+      image: 'quay.io/ansible/ansible-devtools-demo-ee:v0.1.0',
       pullPolicy: 'missing'
     }
   };
