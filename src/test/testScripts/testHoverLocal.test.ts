eb59d6d270f7dcd918ce54c9fded5b03b6bd7c47
Priyam Sahoo <priyamsahoo.26@gmail.com>
2021-11-26 09:29:42 +0530

    adjust hover test script to use dummy collection module

--- src/test/testScripts/testHoverLocal.test.ts
+++ src/test/testScripts/testHoverLocal.test.ts
@@ -63,16 +63,24 @@ describe('TEST FOR HOVER IN LOCAL ENVIRONMENT (without ee)', () => {
       await sleep(1000);
     });
 
-    it('should hover over module name', async () => {
-      await testHover(docUri1, new vscode.Position(4, 7), [
+    it('should hover over builtin module name', async () => {
+      await testHover(docUri1, new vscode.Position(5, 7), [
         {
-          contents: ['Prefix-Lists resource module for VyOS'],
+          contents: ['Print statements during execution'],
+        },
+      ]);
+    });
+
+    it('should hover over collection module name', async () => {
+      await testHover(docUri1, new vscode.Position(9, 7), [
+        {
+          contents: ['Test module'],
         },
       ]);
     });
 
     it('should hover over task keyword', async () => {
-      await testHover(docUri1, new vscode.Position(15, 7), [
+      await testHover(docUri1, new vscode.Position(25, 7), [
         {
           contents: [
             'Name of variable that will contain task status and module return data.',
@@ -88,26 +96,44 @@ describe('TEST FOR HOVER IN LOCAL ENVIRONMENT (without ee)', () => {
       await sleep(1000);
     });
 
-    it('should hover over module option (config)', async () => {
-      await testHover(docUri1, new vscode.Position(5, 9), [
+    it('should hover over buitin module option', async () => {
+      await testHover(docUri1, new vscode.Position(6, 9), [
         {
-          contents: ['A list of prefix-list options'],
+          contents: [
+            'The customized message that is printed. If omitted, prints a generic message.',
+          ],
         },
       ]);
     });
 
-    it('should hover over module sub-option (config -> afi)', async () => {
-      await testHover(docUri1, new vscode.Position(6, 13), [
+    it('should hover over collection module option (opt_1)', async () => {
+      await testHover(docUri1, new vscode.Position(10, 9), [
         {
-          contents: ['The Address Family Indicator (AFI) for the prefix-lists'],
+          contents: ['Option 1'],
         },
       ]);
     });
 
-    it('should hover over module sub-option (config -> prefix_lists -> entries -> description)', async () => {
-      await testHover(docUri1, new vscode.Position(14, 21), [
+    it('should hover over collection module sub-option (opt_1 -> sub_opt_1)', async () => {
+      await testHover(docUri1, new vscode.Position(14, 13), [
         {
-          contents: ['A brief text description for the prefix list rule'],
+          contents: ['Sub option 1'],
+        },
+      ]);
+    });
+
+    it('should hover over collection module sub-option (opt_1 -> sub_opt_2 -> sub_sub_opt_2)', async () => {
+      await testHover(docUri1, new vscode.Position(17, 17), [
+        {
+          contents: ['Sub sub option 2'],
+        },
+      ]);
+    });
+
+    it('should hover over collection module sub-option (opt_1 -> sub_opt_2 -> sub_sub_opt_2 -> sub_sub_sub_opt_1)', async () => {
+      await testHover(docUri1, new vscode.Position(19, 21), [
+        {
+          contents: ['Sub sub sub option 1'],
         },
       ]);
     });
