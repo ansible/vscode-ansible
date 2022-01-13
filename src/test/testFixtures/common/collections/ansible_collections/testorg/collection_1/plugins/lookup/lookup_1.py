fb5d7a61c475fa0118b7cbf6ddc429f4953256da
Priyam Sahoo <priyamsahoo.26@gmail.com>
2022-01-13 19:35:51 +0530

    make dummy collection name consistent throughout the code base

--- src/test/testFixtures/common/collections/ansible_collections/testorg/collection_1/plugins/lookup/lookup_1.py
+++ src/test/testFixtures/common/collections/ansible_collections/testorg/collection_1/plugins/lookup/lookup_1.py
@@ -32,7 +32,7 @@ DOCUMENTATION = """
 EXAMPLES = """
     - name: Retrieve a value deep inside a using a path
       ansible.builtin.set_fact:
-        value: "{{ lookup('testorg.coll_1.lookup_1', var1, var2) }}"
+        value: "{{ lookup('testorg.collection_1.lookup_1', var1, var2) }}"
     """
 
 RETURN = """
