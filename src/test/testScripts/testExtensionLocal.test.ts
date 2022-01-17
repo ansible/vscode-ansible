1a8b68d98db58c179818cfdfd23da24ce76a023b
Priyam Sahoo <priyamsahoo.26@gmail.com>
2022-01-17 18:44:51 +0530

    fix typo in test script name

--- src/test/testScripts/testExtensionLocal.test.ts
+++ src/test/testScripts/testExtensionLocal.test.ts
@@ -1,4 +1,4 @@
-import { testDiagnosticsAnsibleLocal } from './diagnostics/testDiagnoticsAnsibleLocal.test';
+import { testDiagnosticsAnsibleLocal } from './diagnostics/testDiagnosticsAnsibleLocal.test';
 import { testHoverLocal } from './hover/testHoverLocal.test';
 
 describe('TEST EXTENSION IN LOCAL ENVIRONMENT', () => {
