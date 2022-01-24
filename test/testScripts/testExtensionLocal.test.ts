e5f2970330ef4600fabaffa5e844c0c1d7a56b7d
Priyam Sahoo <priyamsahoo.26@gmail.com>
2022-01-24 19:53:04 +0530

    fix code to run yaml diagnostic e2e-test

--- test/testScripts/testExtensionLocal.test.ts
+++ test/testScripts/testExtensionLocal.test.ts
@@ -5,5 +5,5 @@ import { testHoverLocal } from './hover/testHoverLocal.test';
 describe('TEST EXTENSION IN LOCAL ENVIRONMENT', () => {
   testHoverLocal();
   testDiagnosticsAnsibleLocal();
-  testDiagnosticsYAMLLocal;
+  testDiagnosticsYAMLLocal();
 });
