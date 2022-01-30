31f89b42496dc074a180fecc460ab800c88a46a0
Sorin Sbarnea <ssbarnea@redhat.com>
2022-01-30 11:37:34 +0000

    Use bashate with shells scripts

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -4,16 +4,16 @@
 set -ex
 # we don't want the outdated ubuntu blend of ansible
 if [ "$RUNNER_OS" == "Linux" ]; then
-  sudo apt-get remove -y ansible
+    sudo apt-get remove -y ansible
 fi
 
 pipx install pre-commit
 # see https://github.com/pypa/pipx/issues/88
 pipx install ansible-lint
 if [ "$RUNNER_OS" != "Windows" ]; then
-  pipx inject --include-apps ansible-lint yamllint ansible-core
-  ansible-lint --version
-  ansible --version
+    pipx inject --include-apps ansible-lint yamllint ansible-core
+    ansible-lint --version
+    ansible --version
 fi
 
 npm config set fund false
