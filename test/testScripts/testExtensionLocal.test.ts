723844f941ac6795c106c69587493ba20115a1aa
Priyam Sahoo <priyamsahoo.26@gmail.com>
2022-01-31 16:50:00 +0530

    update test launcher structure to put tests for both (ee and without ee)

--- test/testScripts/testExtensionLocal.test.ts
+++ test/testScripts/testExtensionLocal.test.ts
@@ -1,9 +1,25 @@
+import { updateSettings } from '../helper';
 import { testDiagnosticsAnsibleLocal } from './diagnostics/testDiagnosticsAnsibleLocal.test';
 import { testDiagnosticsYAMLLocal } from './diagnostics/testDiagnosticsYAMLLocal.test';
+import { testHoverEE } from './hover/testHoverEE.test';
 import { testHoverLocal } from './hover/testHoverLocal.test';
 
-describe('TEST EXTENSION IN LOCAL ENVIRONMENT', () => {
-  testHoverLocal();
-  testDiagnosticsAnsibleLocal();
-  testDiagnosticsYAMLLocal();
+describe('END-TO-END TEST SUITE FOR REDHAT.ANSIBLE EXTENSION', () => {
+  describe('TEST EXTENSION IN LOCAL ENVIRONMENT', () => {
+    testHoverLocal();
+    testDiagnosticsAnsibleLocal();
+    testDiagnosticsYAMLLocal();
+  });
+
+  describe('TEST EXTENSION IN EXECUTION ENVIRONMENT', () => {
+    before(async () => {
+      await updateSettings('executionEnvironment.enabled', true);
+    });
+
+    after(async () => {
+      await updateSettings('executionEnvironment.enabled', false); // Revert back the default setting
+    });
+
+    testHoverEE();
+  });
 });
