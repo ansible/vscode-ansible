63f5958ba6b9a8fe00feeb7d163c47ff6a1bad09
Priyam Sahoo <42550351+priyamsahoo@users.noreply.github.com>
2022-02-04 08:08:09 +0530

    Add tests for hoverProvider (#199)
    * add test fixtures for hoverProvider test
    
    * add script to test hoverProvider
    
    * restructure and put hoverProvider test script at right place
    
    * increase default timeout for tests
    
    * rectify the docUri to create TextDocument
    
    * add function to create test workspace manager
    
    * fix issues from pre-commit
    
    * add motivation as comment to increase the timeout of mocha tests
    
    * add jsdoc to convey the purpose of createTestWorkspaceManager function
    
    * remove unnecessary files
    
    Co-authored-by: Ganesh Nalawade <ganesh634@gmail.com>

---
+++ test/testScripts/providers/hoverTest.test.ts
@@ -0,0 +1,181 @@
+import { expect } from 'chai';
+import { createTestWorkspaceManager, getDoc } from '../../utils/helper';
+import { doHover } from '../../../src/providers/hoverProvider';
+import { Position } from 'vscode-languageserver';
+
+describe('doHover()', () => {
+  const workspaceManager = createTestWorkspaceManager();
+
+  describe('Play keywords hover', () => {
+    const tests = [
+      {
+        word: 'name',
+        position: { line: 0, character: 4 } as Position,
+        doc: 'Identifier. Can be used for documentation, or in tasks/handlers.',
+      },
+      {
+        word: 'host',
+        position: { line: 1, character: 4 } as Position,
+        doc: 'A list of groups, hosts or host pattern that translates into a list of hosts that are the play’s target.',
+      },
+      {
+        word: 'tasks',
+        position: { line: 3, character: 4 } as Position,
+        doc: 'Main list of tasks to execute in the play, they run after roles and before post_tasks.',
+      },
+    ];
+
+    tests.forEach(({ word, position, doc }) => {
+      it(`should provide hovering for '${word}'`, async function () {
+        const textDoc = await getDoc('hover/tasks.yml');
+        const context = workspaceManager.getContext(textDoc.uri);
+
+        const actualHover = await doHover(
+          textDoc,
+          position,
+          await context.docsLibrary
+        );
+        expect(actualHover.contents['value']).includes(doc);
+      });
+    });
+  });
+
+  describe('Task keywords hover', () => {
+    const tests = [
+      {
+        word: 'register',
+        position: { line: 6, character: 8 } as Position,
+        doc: 'Name of variable that will contain task status and module return data.',
+      },
+    ];
+
+    tests.forEach(({ word, position, doc }) => {
+      it(`should provide hovering for '${word}'`, async function () {
+        const textDoc = await getDoc('hover/tasks.yml');
+        const context = workspaceManager.getContext(textDoc.uri);
+
+        const actualHover = await doHover(
+          textDoc,
+          position,
+          await context.docsLibrary
+        );
+        expect(actualHover.contents['value']).includes(doc);
+      });
+    });
+  });
+
+  describe('Block keywords hover', () => {
+    const tests = [
+      {
+        word: 'become',
+        position: { line: 11, character: 8 } as Position,
+        doc: 'Boolean that controls if privilege escalation is used or not on Task execution. Implemented by the become plugin.',
+      },
+    ];
+
+    tests.forEach(({ word, position, doc }) => {
+      it(`should provide hovering for '${word}'`, async function () {
+        const textDoc = await getDoc('hover/tasks.yml');
+        const context = workspaceManager.getContext(textDoc.uri);
+
+        const actualHover = await doHover(
+          textDoc,
+          position,
+          await context.docsLibrary
+        );
+        expect(actualHover.contents['value']).includes(doc);
+      });
+    });
+  });
+
+  describe('Role keywords hover', () => {
+    const tests = [
+      {
+        word: 'tags',
+        position: { line: 6, character: 8 } as Position,
+        doc: 'Tags applied to the task or included tasks, this allows selecting subsets of tasks from the command line.',
+      },
+    ];
+
+    tests.forEach(({ word, position, doc }) => {
+      it(`should provide hovering for '${word}'`, async function () {
+        const textDoc = await getDoc('hover/roles.yml');
+        const context = workspaceManager.getContext(textDoc.uri);
+
+        const actualHover = await doHover(
+          textDoc,
+          position,
+          await context.docsLibrary
+        );
+        expect(actualHover.contents['value']).includes(doc);
+      });
+    });
+  });
+
+  describe('Module name and options hover', () => {
+    const tests = [
+      {
+        word: 'ansible.builtin.debug',
+        position: { line: 4, character: 8 } as Position,
+        doc: 'Print statements during execution',
+      },
+      {
+        word: 'ansible.builtin.debug -> msg',
+        position: { line: 5, character: 10 } as Position,
+        doc: 'The customized message that is printed. If omitted, prints a generic message.',
+      },
+    ];
+
+    tests.forEach(({ word, position, doc }) => {
+      it(`should provide hovering for '${word}'`, async function () {
+        const textDoc = await getDoc('hover/tasks.yml');
+        const context = workspaceManager.getContext(textDoc.uri);
+
+        const actualHover = await doHover(
+          textDoc,
+          position,
+          await context.docsLibrary
+        );
+        expect(actualHover.contents['value']).includes(doc);
+      });
+    });
+  });
+
+  describe('No hover', () => {
+    it('should not provide hovering for values', async function () {
+      const textDoc = await getDoc('hover/tasks.yml');
+      const context = workspaceManager.getContext(textDoc.uri);
+
+      const actualHover = await doHover(
+        textDoc,
+        { line: 13, character: 24 } as Position,
+        await context.docsLibrary
+      );
+      expect(actualHover).to.be.null;
+    });
+
+    it('should not provide hovering for improper module name and options', async function () {
+      const textDoc = await getDoc('hover/tasks.yml');
+      const context = workspaceManager.getContext(textDoc.uri);
+
+      const actualHover = await doHover(
+        textDoc,
+        { line: 13, character: 8 } as Position,
+        await context.docsLibrary
+      );
+      expect(actualHover).to.be.null;
+    });
+
+    it('should not provide hovering for improper module option', async function () {
+      const textDoc = await getDoc('hover/tasks.yml');
+      const context = workspaceManager.getContext(textDoc.uri);
+
+      const actualHover = await doHover(
+        textDoc,
+        { line: 14, character: 10 } as Position,
+        await context.docsLibrary
+      );
+      expect(actualHover).to.be.null;
+    });
+  });
+});
--- test/utils/helper.ts
+++ test/utils/helper.ts
@@ -1,15 +1,30 @@
 import { TextDocument } from 'vscode-languageserver-textdocument';
 import * as path from 'path';
 import { promises as fs } from 'fs';
+import { WorkspaceManager } from '../../src/services/workspaceManager';
+import { createConnection } from 'vscode-languageserver/node';
 
 export async function getDoc(filename: string): Promise<TextDocument> {
   const file = await fs.readFile(path.resolve('test', 'data', filename), {
     encoding: 'utf8',
   });
-  return TextDocument.create('uri', 'ansible', 1, file);
+  const docUri = path.resolve('test', 'data', filename).toString();
+  return TextDocument.create(docUri, 'ansible', 1, file);
 }
 
 export function isWindows(): boolean {
   // win32 applies to x64 arch too, is the platform name
   return process.platform === 'win32';
+}
+
+/**
+ * A function that initiates the connection object with ipc that can be used to create a workspace manager for testing purposes
+ * @returns {WorkspaceManager} object to serve as a workspace manager for testing purposes
+ */
+export function createTestWorkspaceManager(): WorkspaceManager {
+  process.argv.push('--node-ipc');
+  const connection = createConnection();
+  const workspaceManager = new WorkspaceManager(connection);
+
+  return workspaceManager;
 }
