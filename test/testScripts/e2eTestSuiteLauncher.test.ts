69ca75661ae74ea0c9ec69b223a111b1039a3ba7
Ganesh B. Nalawade <ganesh634@gmail.com>
2022-04-14 14:29:28 +0530

    uncomment w/o test-ee changes

--- test/testScripts/e2eTestSuiteLauncher.test.ts
+++ test/testScripts/e2eTestSuiteLauncher.test.ts
@@ -5,32 +5,29 @@ import { testHoverEE } from "./hover/testWithEE.test";
 import { testHoverWithoutEE } from "./hover/testWithoutEE.test";
 
 describe("END-TO-END TEST SUITE FOR REDHAT.ANSIBLE EXTENSION", () => {
-  // describe("TEST EXTENSION IN LOCAL ENVIRONMENT", () => {
-  //   before(async () => {
-  //     await updateSettings("trace.server", "verbose", "ansibleServer");
-  //   });
+  describe("TEST EXTENSION IN LOCAL ENVIRONMENT", () => {
+    before(async () => {
+      await updateSettings("trace.server", "verbose", "ansibleServer");
+    });
 
-  //   after(async () => {
-  //     await updateSettings("trace.server", "off", "ansibleServer"); // Revert back the default settings
-  //   });
+    after(async () => {
+      await updateSettings("trace.server", "off", "ansibleServer"); // Revert back the default settings
+    });
 
-  //   testHoverWithoutEE();
-  //   testDiagnosticsAnsibleWithoutEE();
-  //   testDiagnosticsYAMLWithoutEE();
-  // });
+    testHoverWithoutEE();
+    testDiagnosticsAnsibleWithoutEE();
+    testDiagnosticsYAMLWithoutEE();
+  });
 
   describe("TEST EXTENSION IN EXECUTION ENVIRONMENT", () => {
     before(async () => {
       await updateSettings("trace.server", "verbose", "ansibleServer");
       await updateSettings("executionEnvironment.enabled", true);
-      await updateSettings("executionEnvironment.containerEngine", "podman");
-      await updateSettings("executionEnvironment.image", "quay.io/ansible/network-ee:stable-2.12");
     });
 
     after(async () => {
       await updateSettings("trace.server", "off", "ansibleServer"); // Revert back the default settings
       await updateSettings("executionEnvironment.enabled", false);
-      await updateSettings("executionEnvironment.containerEngine", "auto");
     });
 
     testHoverEE();
