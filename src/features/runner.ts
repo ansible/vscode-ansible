1804f422d3568aecd3feddbce9516ed3e61da7ef
Thomas Arringe <ta@shrekt.se>
2022-11-19 23:28:04 +0100

    Added reuse terminal option

--- src/features/runner.ts
+++ src/features/runner.ts
@@ -125,11 +125,13 @@ export class AnsiblePlaybookRunProvider {
   private createTerminal(
     runEnv: NodeJS.ProcessEnv | undefined
   ): vscode.Terminal {
-    const reuse_terminal = vscode.window.terminals.find(
-      (terminal) => terminal.name === "Ansible Terminal"
-    );
-    if (reuse_terminal) {
-      return reuse_terminal as vscode.Terminal;
+    if (vscode.workspace.getConfiguration("ansible.ansible").reuseTerminal) {
+      const reuse_terminal = vscode.window.terminals.find(
+        (terminal) => terminal.name === "Ansible Terminal"
+      );
+      if (reuse_terminal) {
+        return reuse_terminal as vscode.Terminal;
+      }
     }
     const terminal = vscode.window.createTerminal({
       name: "Ansible Terminal",
