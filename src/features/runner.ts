1c6b2939c8151a62bde31d01f86bf6cbbd815f90
egnirra <37709886+egnirra@users.noreply.github.com>
2022-11-21 07:55:45 +0100

    Enables reuse of existing ansible terminals (#683)
    Co-authored-by: Thomas Arringe <thomas.arringe@fouredge.se>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ganesh Nalawade <ganesh634@gmail.com>

--- src/features/runner.ts
+++ src/features/runner.ts
@@ -125,6 +125,12 @@ export class AnsiblePlaybookRunProvider {
   private createTerminal(
     runEnv: NodeJS.ProcessEnv | undefined
   ): vscode.Terminal {
+    const reuse_terminal = vscode.window.terminals.find(
+      (terminal) => terminal.name === "Ansible Terminal"
+    );
+    if (reuse_terminal) {
+      return reuse_terminal as vscode.Terminal;
+    }
     const terminal = vscode.window.createTerminal({
       name: "Ansible Terminal",
       env: runEnv,
