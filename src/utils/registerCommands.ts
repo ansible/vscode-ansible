a82c695602554c3ee0e602d84749ef7070ef02ce
Ganesh Nalawade <ganesh634@gmail.com>
2022-12-27 23:17:09 +0530

    Add Red Hat telemetry gathering (#732)
    * Add Red Hat telemetry gathering
    
    *  Add setting to enable/disable telemetry gathering
    *  Add telemtry event gathering feature
    *  Refer USAGE_DATA.md file to identify which
       telemetry information is gathered
    *  Refactor the ansible command invocation logic to allow
       registering command for telmetry gathering
    
    * chore: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * revert vscode min version
    
    * fix lint issues
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/utils/registerCommands.ts
@@ -0,0 +1,37 @@
+import { commands, ExtensionContext, window } from "vscode";
+import { TelemetryManager } from "./telemetryUtils";
+
+/**
+ * Register a command with the given name and async function
+ *
+ * Displays errors raised by the function as error messages,
+ * and sends telemetry when the command succeeds or fails.
+ *
+ * @param context the extension context
+ * @param commandName the name of the command to register
+ * @param commandAction the async function to run when the command is called
+ * @param skipSuccess whether the success of the command should be reported
+ */
+export async function registerCommandWithTelemetry(
+  context: ExtensionContext,
+  telemetry: TelemetryManager,
+  commandName: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  commandAction: (thisArg?: any) => Promise<any>,
+  skipSuccess?: boolean
+): Promise<void> {
+  context.subscriptions.push(
+    commands.registerCommand(commandName, async () => {
+      try {
+        await commandAction();
+        if (!skipSuccess) {
+          telemetry.sendCommandSucceededTelemetry(commandName);
+        }
+      } catch (e) {
+        const msg = e instanceof Error ? e.message : (e as string);
+        window.showErrorMessage(msg);
+        telemetry.sendCommandFailedTelemetry(commandName, msg);
+      }
+    })
+  );
+}
