078f42e7cc2aeed9a2653941874e2f286f314dc5
Priyam Sahoo <priyamsahoo.26@gmail.com>
2022-12-28 16:57:16 +0530

    update to valid keys

--- src/features/ansibleMetaData.ts
+++ src/features/ansibleMetaData.ts
@@ -98,7 +98,7 @@ export class MetadataManager {
           this.metadataStatusBarItem.show();
           const eventData: ansibleMetadataEvent = {
             ansibleVersion:
-              ansibleMetaData.metaData["ansible information"]["version"],
+              ansibleMetaData.metaData["ansible information"]["core version"],
             eeEnabled: ansibleMetaData.eeEnabled,
           };
           if (ansibleMetaData.ansibleLintPresent) {
