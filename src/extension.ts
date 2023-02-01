9006755aa9ee18389ffc3f81b10c8999ecb42e9c
Ganesh Nalawade <ganesh634@gmail.com>
2023-02-01 16:47:19 +0530

    Add fix to remove prompt from suggestion (#770)
    As per newer request contract for wisdom service
    add `-name:` prefix as part of the prompt and
    the context as everything before the prompt
    
    Sent userId and suggestionId as part of the request
     to service

--- src/extension.ts
+++ src/extension.ts
@@ -86,10 +86,13 @@ export async function activate(context: ExtensionContext): Promise<void> {
   wisdomManager = new WisdomManager(context, client, extSettings, telemetry);
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("extension.onEnter", () => {
-      vscode.commands.executeCommand("default:type", { text: "\n" });
-      setKeyInput("enter");
-    })
+    vscode.commands.registerCommand(
+      WisdomCommands.WISDOM_SUGGESTION_ON_ENTER,
+      () => {
+        vscode.commands.executeCommand("default:type", { text: "\n" });
+        setKeyInput("enter");
+      }
+    )
   );
 
   context.subscriptions.push(
