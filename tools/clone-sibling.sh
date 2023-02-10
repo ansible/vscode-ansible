55da1a1f78a4008289ce0b54be8da431c879403d
Priyam Sahoo <42550351+priyamsahoo@users.noreply.github.com>
2023-02-10 17:38:31 +0530

    Install yarn 1.10.2 (#779)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tools/clone-sibling.sh
+++ tools/clone-sibling.sh
@@ -3,8 +3,13 @@ set -euo pipefail
 
 if [[ -d ../ansible-language-server ]]; then
     echo "Ansible Language Server already cloned"
-    exit 0
 else
     echo "Cloning Ansible Language Server"
     git clone https://github.com/ansible/ansible-language-server ../ansible-language-server
 fi
+
+# install dependencies of ansible-language-server before linking it to vscode-ansible
+echo "Installing deps and compiling Ansible Language Server"
+cd ../ansible-language-server
+npm ci
+cd ../vscode-ansible
