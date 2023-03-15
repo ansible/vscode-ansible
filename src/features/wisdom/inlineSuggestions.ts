d7ebe80c55d55f858ce94ca70a8fe19cdb06a8d1
Ganesh B. Nalawade <ganesh634@gmail.com>
2023-03-15 23:23:52 +0530

    remove duplicate activity id from event

--- src/features/wisdom/inlineSuggestions.ts
+++ src/features/wisdom/inlineSuggestions.ts
@@ -232,7 +232,7 @@ export async function inlineSuggestionCommitHandler(
   vscode.commands.executeCommand("editor.action.inlineSuggest.commit");
 
   // Send feedback for accepted suggestion
-  await inlineSuggestionUserActionHandler(suggestionId, true);
+  await inlineSuggestionUserActionHandler(document, suggestionId, true);
 }
 
 export async function inlineSuggestionHideHandler(
