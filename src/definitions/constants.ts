ba96c78002770dc97c5e7af6643b46deece3d761
Sumit Jaiswal <sjaiswal@redhat.com>
2023-03-24 01:45:43 +0530

    To add the support for Wisdom api versioning (#823)
    * api versioning
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * api versioning
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * update changes
    
    * chore: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    Co-authored-by: Ganesh Nalawade <ganesh634@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/definitions/constants.ts
+++ src/definitions/constants.ts
@@ -20,9 +20,15 @@ export namespace WisdomCommands {
     "ansible.wisdom.clearTrainingMatches";
 }
 
-export const WISDOM_SUGGESTION_COMPLETION_URL = "/ai/completions/";
-export const WISDOM_SUGGESTION_FEEDBACK_URL = "/ai/feedback/";
-export const WISDOM_SUGGESTION_ATTRIBUTIONS_URL = "/ai/attributions/";
+export const WISDOM_API_VERSION = "v0/";
+export const WISDOM_SUGGESTION_COMPLETION_URL =
+  WISDOM_API_VERSION.concat("ai/completions/");
+export const WISDOM_SUGGESTION_FEEDBACK_URL =
+  WISDOM_API_VERSION.concat("ai/feedback/");
+export const WISDOM_ME_AUTH_URL = "/api/"
+  .concat(WISDOM_API_VERSION)
+  .concat("me/");
+export const WISDOM_SUGGESTION_ATTRIBUTIONS_URL = `${WISDOM_API_VERSION}/ai/attributions/`;
 
 export const WISDOM_FEEDBACK_FORM_URL =
   "https://redhatdg.co1.qualtrics.com/jfe/form/SV_e99JvA2DHp5UlWC";
