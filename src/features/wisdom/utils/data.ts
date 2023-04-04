f231ad43ddbed3c49fea09dff49cb00f56d9e0f3
Ganesh B. Nalawade <ganesh634@gmail.com>
2023-04-04 18:01:07 +0530

    fix review comments

--- src/features/wisdom/utils/data.ts
+++ src/features/wisdom/utils/data.ts
@@ -4,9 +4,9 @@ import * as yaml from "yaml";
 export function shouldRequestInlineSuggestions(
   documentContent: string
 ): boolean {
-  let parsedAnsibledocument = undefined;
+  let parsedAnsibleDocument = undefined;
   try {
-    parsedAnsibledocument = yaml.parse(documentContent, { keepCstNodes: true });
+    parsedAnsibleDocument = yaml.parse(documentContent, { keepCstNodes: true });
   } catch (err) {
     vscode.window.showErrorMessage(
       `Project Wisdom expects valid YAML synatax to provide inline suggestions. Error: ${err}`
@@ -14,19 +14,19 @@ export function shouldRequestInlineSuggestions(
     return false;
   }
   // if the file is empty, we don't want to request inline suggestions
-  if (parsedAnsibledocument === undefined) {
+  if (parsedAnsibleDocument === undefined) {
     return false;
   }
 
   // check if YAML is a list, if not it is not a valid Ansible document
   if (
-    typeof parsedAnsibledocument === "object" &&
-    !Array.isArray(parsedAnsibledocument)
+    typeof parsedAnsibleDocument === "object" &&
+    !Array.isArray(parsedAnsibleDocument)
   ) {
     return false;
   }
 
-  const lastObject = parsedAnsibledocument[parsedAnsibledocument.length - 1];
+  const lastObject = parsedAnsibleDocument[parsedAnsibleDocument.length - 1];
   if (typeof lastObject !== "object") {
     return false;
   }
