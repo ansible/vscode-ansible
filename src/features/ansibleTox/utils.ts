0efe2c0991b5f70bd283501ea209cacd48edbdee
Ganesh B. Nalawade <ganesh634@gmail.com>
2023-05-21 09:44:48 +0200

    fix test failure

--- src/features/ansibleTox/utils.ts
+++ src/features/ansibleTox/utils.ts
@@ -3,26 +3,32 @@ import * as path from "path";
 
 export function findProjectDir() {
   const docUri = vscode.window.activeTextEditor?.document.uri;
+  if (docUri) {
+    const workspace = vscode.workspace.getWorkspaceFolder(docUri);
+    if (workspace) {
+      const folder = workspace.uri.fsPath;
+      console.log(`tox workspace folder: ${folder}`);
+      return folder;
+    } else {
+      const docDir = path.dirname(docUri.fsPath);
+      console.log(`tox doc path: ${docUri.fsPath} -> ${docDir}`);
+      return docDir;
+    }
+  } else {
+    console.log("tox ansible no active editor found");
 
-  if (!docUri) {
-    throw new Error("No active editor found.");
+    // Fixme: this is not working for multi-root workspaces
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (workspaceFolders) {
+      const rootPath = workspaceFolders[0].uri.fsPath;
+      console.log("Workspace root directory:", rootPath);
+      return rootPath;
+    }
   }
-
-  const workspace = vscode.workspace.getWorkspaceFolder(docUri);
-
-  if (workspace) {
-    const folder = workspace.uri.fsPath;
-    console.log(`tox workspace folder: ${folder}`);
-    return folder;
-  }
-
-  const docDir = path.dirname(docUri.fsPath);
-  console.log(`tox doc path: ${docUri.fsPath} -> ${docDir}`);
-  return docDir;
 }
 
 export function getTerminal(
-  projDir: string = findProjectDir(),
+  projDir: string | undefined = findProjectDir(),
   name = "Ansible Tox"
 ): vscode.Terminal {
   for (const terminal of vscode.window.terminals) {
