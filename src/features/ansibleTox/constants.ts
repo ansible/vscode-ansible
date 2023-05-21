894b66d221ef4b9998f70c3aebb3b33850b20510
Ganesh Nalawade <ganesh634@gmail.com>
2023-05-21 18:05:38 +0000

    Add support for tox-ansible plugin test discovery (#902)
    * Add support for tox-ansible plugin test discovery
    
    *  To provide Ansible test matrix using tox in the test
       explorer, install (tox-ansible)[https://pypi.org/project/tox-ansible/].
    *  Add `tox-ansible.ini` file in root folder of workspace which should have
       a section called `[ansible]`.
    *  Run test from the test explorer.
    
    Known issues:
    *  Test run indicates success in explorer window
       immediately after test start running without
       waiting for the actual test result
    *  Test explorer refresh does not work proprely and
       the create the test in explorer reload the VSCode window
    
    * fix test failure

---
+++ src/features/ansibleTox/constants.ts
@@ -0,0 +1,6 @@
+export const TOX_TYPE = "Ansible Tox";
+export const ANSIBLE_TOX_FILE_NAME = "tox-ansible.ini";
+export const ANSIBLE_TOX_LIST_ENV_COMMAND =
+  "tox -l --ansible --conf tox-ansible.ini";
+
+export const ANSIBLE_TOX_RUN_COMMAND = "tox -e";
---
+++ src/features/ansibleTox/utils.ts
@@ -0,0 +1,50 @@
+import * as vscode from "vscode";
+import * as path from "path";
+
+export function findProjectDir() {
+  const docUri = vscode.window.activeTextEditor?.document.uri;
+  if (docUri) {
+    const workspace = vscode.workspace.getWorkspaceFolder(docUri);
+    if (workspace) {
+      const folder = workspace.uri.fsPath;
+      console.log(`tox workspace folder: ${folder}`);
+      return folder;
+    } else {
+      const docDir = path.dirname(docUri.fsPath);
+      console.log(`tox doc path: ${docUri.fsPath} -> ${docDir}`);
+      return docDir;
+    }
+  } else {
+    console.log("tox ansible no active editor found");
+
+    // Fixme: this is not working for multi-root workspaces
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (workspaceFolders) {
+      const rootPath = workspaceFolders[0].uri.fsPath;
+      console.log("Workspace root directory:", rootPath);
+      return rootPath;
+    }
+  }
+}
+
+export function getTerminal(
+  projDir: string | undefined = findProjectDir(),
+  name = "Ansible Tox"
+): vscode.Terminal {
+  for (const terminal of vscode.window.terminals) {
+    if (terminal.name === name) {
+      return terminal;
+    }
+  }
+  return vscode.window.createTerminal({ cwd: projDir, name: name });
+}
+
+export function getRootParentLabelDesc(test: vscode.TestItem): string {
+  let root = test;
+
+  while (root.parent !== undefined) {
+    root = root.parent;
+  }
+
+  return `${root.label} ${root.description}`;
+}
