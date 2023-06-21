fdfef377ca1708959f6b76c051e1e582a412a8cc
Ganesh Nalawade <ganesh634@gmail.com>
2023-06-21 12:37:05 +0530

    Lightspeed UI feedback and sentiment (#924)
    * Lightspeed UI feedback and sentiment
    
    * update tsconfig
    
    * remove unwanted files
    
    * chore: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * chore: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * CSS styles are now working
    
    * Update styles
    
    * Update webview style and appearance
    
    * chore: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove file media/codicon.css as it is not required
    
    * more changes
    
    * Fix review comments
    
    * UI update for sentiment selection
    
    * Rebase
    
    * More fixes and webview addition
    
    * add deleted file
    
    * remove hook id
    
    * chore: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add event listener for feedback and send to lightspeed service
    
    * Removed duplicalte 'value' for sentiment selection
    
    * remove unwanted changes
    
    * update package.json with webpack script
    
    * revert package.json changes
    
    * update the package path
    
    * comment out ui test and yarn auit check
    
    * run only ui test with increased timeout
    
    * comment test-ui testcases due to failure in vscode-extension-tester
    
    * restructure webview script folder structure
    
    * fix review comments
    
    * Update placeholder description
    
    * update vscode ignore file path
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ajinkya <ajinkyaudgirkar@gmail.com>

---
+++ src/features/lightspeed/feedbackWebviewProvider.ts
@@ -0,0 +1,86 @@
+import {
+  Disposable,
+  Webview,
+  WebviewPanel,
+  window,
+  Uri,
+  ViewColumn,
+} from "vscode";
+import {
+  getWebviewContent,
+  setWebviewMessageListener,
+} from "./utils/feedbackView";
+
+export class LightspeedFeedbackWebviewProvider {
+  public static currentPanel: LightspeedFeedbackWebviewProvider | undefined;
+  private readonly _panel: WebviewPanel;
+  private _disposables: Disposable[] = [];
+
+  private constructor(panel: WebviewPanel, extensionUri: Uri) {
+    this._panel = panel;
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+    // Set the HTML content for the webview panel
+    this._panel.webview.html = this._getWebviewContent(
+      this._panel.webview,
+      extensionUri
+    );
+
+    // Set an event listener to listen for messages passed from the webview context
+    this._setWebviewMessageListener(this._panel.webview);
+  }
+
+  public static render(extensionUri: Uri) {
+    if (LightspeedFeedbackWebviewProvider.currentPanel) {
+      // If the webview panel already exists reveal it
+      LightspeedFeedbackWebviewProvider.currentPanel._panel.reveal(
+        ViewColumn.One
+      );
+    } else {
+      // If a webview panel does not already exist create and show a new one
+      const panel = window.createWebviewPanel(
+        "ansibleLightSpeedFeedback",
+        "Ansible Lightspeed Feedback",
+        ViewColumn.One,
+        {
+          // Enable JavaScript in the webview
+          enableScripts: true,
+          // Restrict the webview to only load resources from the `out` directory
+          localResourceRoots: [
+            Uri.joinPath(extensionUri, "out"),
+            Uri.joinPath(extensionUri, "media"),
+          ],
+        }
+      );
+
+      LightspeedFeedbackWebviewProvider.currentPanel =
+        new LightspeedFeedbackWebviewProvider(panel, extensionUri);
+    }
+  }
+
+  /**
+   * Cleans up and disposes of webview resources when the webview panel is closed.
+   */
+  public dispose() {
+    LightspeedFeedbackWebviewProvider.currentPanel = undefined;
+
+    // Dispose of the current webview panel
+    this._panel.dispose();
+
+    // Dispose of all disposables (i.e. commands) associated with the current webview panel
+    while (this._disposables.length) {
+      const disposable = this._disposables.pop();
+      if (disposable) {
+        disposable.dispose();
+      }
+    }
+  }
+
+  private _getWebviewContent(webview: Webview, extensionUri: Uri) {
+    return getWebviewContent(webview, extensionUri);
+  }
+
+  private async _setWebviewMessageListener(webview: Webview) {
+    await setWebviewMessageListener(webview, this._disposables);
+  }
+}
