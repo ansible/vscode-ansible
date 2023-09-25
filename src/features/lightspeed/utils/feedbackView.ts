323233c050a6974ba994b31cd318a9ab3b4675d7
Ganesh Nalawade <ganesh634@gmail.com>
2023-09-25 19:48:34 +0530

    Add a checkbox while sharing Lightspeed sentiment/issue feedback (#980)
    * Add a checkbox which ensure user selects and agree to sharing
      the feedback data with Red Hat and IBM for sentiment and issue
      feedback

--- src/features/lightspeed/utils/feedbackView.ts
+++ src/features/lightspeed/utils/feedbackView.ts
@@ -17,56 +17,60 @@ export function getWebviewContent(webview: Webview, extensionUri: Uri) {
   const nonce = getNonce();
 
   return /*html*/ `
-        <!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="${styleUri}">
-            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-            <title>Ansible Lightspeed Feedback!</title>
-          </head>
-          <body>
-            <form id="feedback-form">
-              <section class="component-container">
-                <h3>How was your experience?</h3>
-                <section class="sentiment-button">
-                  <div class="sentiment-selector">
-                      <input id="very-negative" value=1 type="radio" name="sentiment" />
-                      <label class="sentiment very-negative" for="very-negative"></label>
-                      <input id="negative" value=2 type="radio" name="sentiment" />
-                      <label class="sentiment negative"for="negative"></label>
-                      <input id="neutral" value=3 type="radio" name="sentiment" />
-                      <label class="sentiment neutral"for="neutral"></label>
-                      <input id="positive" value=4 type="radio" name="sentiment" />
-                      <label class="sentiment positive"for="positive"></label>
-                      <input id="very-positive" value=5 type="radio" name="sentiment" />
-                      <label class="sentiment very-positive"for="very-positive"></label>
-                  </div>
-                </section>
-              </section>
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="${styleUri}">
+      <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+      <title>Ansible Lightspeed Feedback!</title>
+    </head>
+    <body>
+      <form id="feedback-form">
+        <section class="component-container">
+          <h3>How was your experience?</h3>
+          <section class="sentiment-button">
+            <div class="sentiment-selector">
+                <input id="very-negative" value=1 type="radio" name="sentiment" />
+                <label class="sentiment very-negative" for="very-negative"></label>
+                <input id="negative" value=2 type="radio" name="sentiment" />
+                <label class="sentiment negative"for="negative"></label>
+                <input id="neutral" value=3 type="radio" name="sentiment" />
+                <label class="sentiment neutral"for="neutral"></label>
+                <input id="positive" value=4 type="radio" name="sentiment" />
+                <label class="sentiment positive"for="positive"></label>
+                <input id="very-positive" value=5 type="radio" name="sentiment" />
+                <label class="sentiment very-positive"for="very-positive"></label>
+            </div>
+          </section>
+        </section>
+        <section class="component-section">
+            <p class="required">Tell us why?</p>
+            <vscode-text-area maxlength="512" cols="29" resize="both" id="sentiment-comment"></vscode-text-area>
+        </section>
+        <section class="component-section">
+            <input type="checkbox" id="sentiment-data-sharing-checkbox">
+            <label for="sentiment-data-sharing-checkbox">I understand that feedback is shared with Red Hat and IBM.</label>
+        </section>
+        <section class="component-section">
+            <vscode-button id="sentiment-submit" disabled>Send</vscode-button>
+        </section>
+        <vscode-divider></vscode-divider>
+        <section class="component-container">
+            <h3>Tell us more</h3>
               <section class="component-section">
-                  <p class="required">Tell us why?</p>
-                  <vscode-text-area maxlength="512" cols="29" resize="both" id="sentiment-comment"></vscode-text-area>
+                <vscode-dropdown id="issue-type-dropdown" class="issue-dropdown">
+                  <vscode-option selected value="select-issue-type">Select Issue type</vscode-option>
+                  <vscode-option value="bug-report">Bug report</vscode-option>
+                  <vscode-option value="feature-request">Feature request</vscode-option>
+                  <vscode-option value="suggestion-feedback">Suggestion feedback</vscode-option>
+                </vscode-dropdown>
               </section>
-              <section class="component-section">
-                  <vscode-button id="sentiment-submit">Send</vscode-button>
-              </section>
-          <vscode-divider></vscode-divider>
-          <section class="component-container">
-              <h3>Tell us more</h3>
-                <section class="component-section">
-                  <vscode-dropdown id="issue-type-dropdown" class="issue-dropdown">
-                    <vscode-option selected value="select-issue-type">Select Issue type</vscode-option>
-                    <vscode-option value="bug-report">Bug report</vscode-option>
-                    <vscode-option value="feature-request">Feature request</vscode-option>
-                    <vscode-option value="suggestion-feedback">Suggestion feedback</vscode-option>
-                  </vscode-dropdown>
-                </section>
-                  <script type="module" nonce="${nonce}" src="${webviewUri}"></script>
-            </form>
-          </body>
-        </html>
+                <script type="module" nonce="${nonce}" src="${webviewUri}"></script>
+          </form>
+    </body>
+  </html>
         `;
 }
 
