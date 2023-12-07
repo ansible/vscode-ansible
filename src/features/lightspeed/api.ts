4f2c0774aa31c7674fba9cd934f7799872c21ea1
Gonéri Le Bouder <goneri@lebouder.net>
2023-12-07 13:58:44 -0500

    lightspeed: a unit-test for the error handling
    Add test coverage for the error handling. The tests use `jest` and can be enabled with:
    
    ```
    yarn add --dev jest
    yarn add --dev ts-jest @types/jest
    yarn test
    ```

--- src/features/lightspeed/api.ts
+++ src/features/lightspeed/api.ts
@@ -18,6 +18,7 @@ import {
 import { LightSpeedAuthenticationProvider } from "./lightSpeedOAuthProvider";
 import { getBaseUri } from "./utils/webUtils";
 import { ANSIBLE_LIGHTSPEED_API_TIMEOUT } from "../../definitions/constants";
+import { retrieve_error } from "./handle_api_error";
 
 export class LightSpeedAPI {
   private axiosInstance: AxiosInstance | undefined;
@@ -112,85 +113,7 @@ export class LightSpeedAPI {
       return response.data;
     } catch (error) {
       const err = error as AxiosError;
-      if (err && "response" in err) {
-        if (err?.response?.status === 401) {
-          vscode.window.showErrorMessage(
-            "User not authorized to access Ansible Lightspeed."
-          );
-        } else if (err?.response?.status === 429) {
-          vscode.window.showErrorMessage(
-            "Too many requests to Ansible Lightspeed. Please try again after some time."
-          );
-        } else if (err?.response?.status === 400) {
-          const responseErrorData = <AxiosError<{ message?: string }>>(
-            err?.response?.data
-          );
-          if (
-            responseErrorData &&
-            responseErrorData.hasOwnProperty("message") &&
-            responseErrorData.message?.includes("Cloudflare")
-          ) {
-            vscode.window.showErrorMessage(
-              `Cloudflare rejected the request. Please contact your administrator.`
-            );
-          } else {
-            vscode.window.showErrorMessage(
-              "Bad Request response. Please try again."
-            );
-          }
-        } else if (err?.response?.status === 403) {
-          const responseErrorData = <AxiosError<{ message?: string }>>(
-            err?.response?.data
-          );
-          if (
-            responseErrorData &&
-            responseErrorData.hasOwnProperty("message") &&
-            responseErrorData.message?.includes("WCA Model ID is invalid")
-          ) {
-            vscode.window.showErrorMessage(
-              `Model ID "${this.settingsManager.settings.lightSpeedService.model}" is invalid. Please contact your administrator.`
-            );
-          } else if (
-            responseErrorData &&
-            responseErrorData.hasOwnProperty("code") &&
-            responseErrorData.code ===
-              "permission_denied__org_ready_user_has_no_seat"
-          ) {
-            vscode.window.showErrorMessage(
-              `You do not have a licensed seat for Ansible Lightspeed and your organization is using the paid commercial service. Contact your Red Hat Organization's administrator for more information on how to get a licensed seat.`
-            );
-          } else if (
-            responseErrorData &&
-            responseErrorData.hasOwnProperty("code") &&
-            responseErrorData.code ===
-              "permission_denied__org_not_ready_because_wca_not_configured"
-          ) {
-            vscode.window.showErrorMessage(
-              `Contact your administrator to configure IBM watsonx Code Assistant model settings for your organization.`
-            );
-          } else {
-            vscode.window.showErrorMessage(
-              `User not authorized to access Ansible Lightspeed.`
-            );
-          }
-        } else if (err?.response?.status.toString().startsWith("5")) {
-          vscode.window.showErrorMessage(
-            "Ansible Lightspeed encountered an error. Try again after some time."
-          );
-        } else {
-          vscode.window.showErrorMessage(
-            `Failed to fetch inline suggestion from Ansible Lightspeed with status code: ${err?.response?.status}. Try again after some time.`
-          );
-        }
-      } else if (err.code === AxiosError.ECONNABORTED) {
-        vscode.window.showErrorMessage(
-          "Ansible Lightspeed connection timeout. Try again after some time."
-        );
-      } else {
-        vscode.window.showErrorMessage(
-          "Failed to fetch inline suggestion from Ansible Lightspeed. Try again after some time."
-        );
-      }
+      vscode.window.showErrorMessage(retrieve_error(err));
       this._completionRequestInProgress = false;
       return {} as CompletionResponseParams;
     } finally {
