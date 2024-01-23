82954daf671ad2a40fa82f0e14472b01dca18d9d
Gonéri Le Bouder <goneri@lebouder.net>
2024-01-23 18:13:39 -0500

    lightspeed/inlineSuggestions: refactoring to simplify the testing (#1055)
    The goal is to break up the logic in two parts:
    1. The code that we use parse the prompt is now isolated in:
      - `getCompletionState()` and
      - `InlineSuggestionState()`.
      It will only return a state that trigger a callback function.
    2. Is the second part, where our `on*` callbacks are finally called
      to trigger potentially a change.
    
    The benefits of this approach:
    
    - Since we parsing of the content and prompt only creates a state without
      any side-effect, it becomes easier to properly test it.
    - Also the callbacks are much smaller and simpler. They become more suitable
      for future unit-test integration.
    
    Also, this PR reduces the number of global variables.

---
+++ src/features/lightspeed/inlineSuggestion/additionalContext.ts
@@ -0,0 +1,105 @@
+import { WorkspaceFolder } from "vscode";
+import * as yaml from "yaml";
+import {
+  IAdditionalContext,
+  IAnsibleFileType,
+  IPlaybookContext,
+  IRoleContext,
+  IRolesContext,
+  IStandaloneTaskContext,
+} from "../../../interfaces/lightspeed";
+import {
+  getVarsFilesContext,
+  getRelativePath,
+  getRolePathFromPathWithinRole,
+  getIncludeVarsContext,
+} from "../utils/data";
+import { lightSpeedManager } from "../../../extension";
+import { getCustomRolePaths } from "../../utils/ansible";
+import { watchRolesDirectory } from "../utils/watchers";
+
+export function getAdditionalContext(
+  parsedAnsibleDocument: yaml.YAMLMap[],
+  documentDirPath: string,
+  documentFilePath: string,
+  ansibleFileType: IAnsibleFileType,
+  workspaceFolders: readonly WorkspaceFolder[] | undefined
+): IAdditionalContext {
+  let workSpaceRoot = undefined;
+  const playbookContext: IPlaybookContext = {};
+  let roleContext: IRoleContext = {};
+  const standaloneTaskContext: IStandaloneTaskContext = {};
+  if (workspaceFolders) {
+    workSpaceRoot = workspaceFolders[0].uri.fsPath;
+  }
+  if (ansibleFileType === "playbook") {
+    const varsFilesContext = getVarsFilesContext(
+      lightSpeedManager,
+      parsedAnsibleDocument,
+      documentDirPath
+    );
+    playbookContext["varInfiles"] = varsFilesContext || {};
+    const rolesCache: IRolesContext = {};
+    if (workSpaceRoot) {
+      // check if roles are installed in the workspace
+      if (!(workSpaceRoot in lightSpeedManager.ansibleRolesCache)) {
+        const rolesPath = getCustomRolePaths(workSpaceRoot);
+        for (const rolePath of rolesPath) {
+          watchRolesDirectory(lightSpeedManager, rolePath, workSpaceRoot);
+        }
+      }
+      // if roles are installed in the workspace, then get the relative path w.r.t. the workspace root
+      if (workSpaceRoot in lightSpeedManager.ansibleRolesCache) {
+        const workspaceRolesCache =
+          lightSpeedManager.ansibleRolesCache[workSpaceRoot];
+        for (const absRolePath in workspaceRolesCache) {
+          const relativeRolePath = getRelativePath(
+            documentDirPath,
+            workSpaceRoot,
+            absRolePath
+          );
+          rolesCache[relativeRolePath] = workspaceRolesCache[absRolePath];
+        }
+      }
+    }
+    if ("common" in lightSpeedManager.ansibleRolesCache) {
+      for (const commonRolePath in lightSpeedManager.ansibleRolesCache) {
+        rolesCache[commonRolePath] =
+          lightSpeedManager.ansibleRolesCache["common"][commonRolePath];
+      }
+    }
+    playbookContext["roles"] = rolesCache;
+  } else if (ansibleFileType === "tasks_in_role") {
+    const roleCache = lightSpeedManager.ansibleRolesCache;
+    const absRolePath = getRolePathFromPathWithinRole(documentFilePath);
+    if (
+      workSpaceRoot &&
+      workSpaceRoot in roleCache &&
+      absRolePath in roleCache[workSpaceRoot]
+    ) {
+      roleContext = roleCache[workSpaceRoot][absRolePath];
+    }
+  }
+  const includeVarsContext =
+    getIncludeVarsContext(
+      lightSpeedManager,
+      parsedAnsibleDocument,
+      documentDirPath,
+      ansibleFileType
+    ) || {};
+
+  if (ansibleFileType === "playbook") {
+    playbookContext.includeVars = includeVarsContext;
+  } else if (ansibleFileType === "tasks_in_role") {
+    roleContext.includeVars = includeVarsContext;
+  } else if (ansibleFileType === "tasks") {
+    standaloneTaskContext.includeVars = includeVarsContext;
+  }
+
+  const additionalContext: IAdditionalContext = {
+    playbookContext: playbookContext,
+    roleContext: roleContext,
+    standaloneTaskContext: standaloneTaskContext,
+  };
+  return additionalContext;
+}
