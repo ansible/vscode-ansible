754b74f43698849140245ccb5e7f44d74d19aa99
Tami Takamiya <ttakamiy@redhat.com>
2024-02-08 13:55:14 -0500

    Redesign ip address assignment logic per platform

--- test/mockLightspeedServer/server.ts
+++ test/mockLightspeedServer/server.ts
@@ -7,7 +7,7 @@ import { openUrl } from "./openUrl";
 const API_VERSION = "v0";
 const API_ROOT = `/api/${API_VERSION}`;
 
-const url = new URL(process.env.TEST_LIGHTSPEED_URL || "http://127.0.0.1:3000");
+const url = new URL("http://localhost:3000");
 
 export default class Server {
   constructor(app: Application) {
