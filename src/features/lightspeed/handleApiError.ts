e9a9a778d5b30da74966b2877cd3613f8e3778f0
Michael Anstis <manstis@redhat.com>
2024-03-25 11:32:43 +0000

    AAP-19228: Refactor error handling in wisdom service and vscode extension
    Incorporates:
    - AAP-19005: Improve error handing for 400 bad request
    - AAP-20093: 'User not authorized to access Ansible Lightspeed' with no explanation

--- src/features/lightspeed/handleApiError.ts
+++ src/features/lightspeed/handleApiError.ts
@@ -1,25 +1,40 @@
 import { AxiosError } from "axios";
+import {
+  ERRORS,
+  ERRORS_UNAUTHORIZED,
+  ERRORS_TOO_MANY_REQUESTS,
+  ERRORS_BAD_REQUEST,
+  ERRORS_UNKNOWN,
+  ERRORS_CONNECTION_TIMEOUT,
+} from "./errors";
 
 export function retrieveError(err: AxiosError): string {
   if (err && "response" in err) {
-    if (err?.response?.status === 401) {
-      return "User not authorized to access Ansible Lightspeed.";
-    } else if (err?.response?.status === 429) {
-      return "Too many requests to Ansible Lightspeed. Please try again after some time.";
-    } else if (err?.response?.status === 400) {
-      const responseErrorData = <AxiosError<{ message?: string }>>(
-        err?.response?.data
-      );
-      if (
-        responseErrorData &&
-        responseErrorData.hasOwnProperty("message") &&
-        responseErrorData.message?.includes("Cloudflare")
-      ) {
-        return `Cloudflare rejected the request. Please contact your administrator.`;
-      } else {
-        return "Bad Request response. Please try again.";
-      }
-    } else if (err?.response?.status === 403) {
+    const responseErrorData = <AxiosError<{ message?: string }>>(
+      err?.response?.data
+    );
+    // Lookup _known_ errors
+    const status: number = err?.response?.status ?? 500;
+    const code: string = responseErrorData.hasOwnProperty("code")
+      ? (responseErrorData.code as string)
+      : "unknown";
+    const message = responseErrorData.hasOwnProperty("message")
+      ? (responseErrorData.message as string)
+      : "unknown";
+    const mappedError = ERRORS.getError(status, code);
+    if (mappedError) {
+      return mappedError.message || message;
+    }
+
+    // If the error is unknown fallback to defaults
+    if (status === 400) {
+      return ERRORS_BAD_REQUEST.message || message;
+    }
+    if (status === 401) {
+      return ERRORS_UNAUTHORIZED.message || message;
+    }
+    if (status === 403) {
+      // Special case where the error is not from the backend service
       if (
         (err?.response?.headers["server"] || "").toLowerCase() === "cloudfront"
       ) {
@@ -29,48 +44,19 @@ export function retrieveError(err: AxiosError): string {
           "line and column where you requested a suggestion."
         );
       } else {
-        const responseErrorData = <AxiosError<{ message?: string }>>(
-          err?.response?.data
-        );
-        if (
-          responseErrorData &&
-          responseErrorData.hasOwnProperty("message") &&
-          responseErrorData.message?.includes("WCA Model ID is invalid")
-        ) {
-          return `Model ID is invalid. Please contact your administrator.`;
-        } else if (responseErrorData) {
-          switch (
-            responseErrorData.hasOwnProperty("code") &&
-            responseErrorData.code
-          ) {
-            case "permission_denied__org_ready_user_has_no_seat": {
-              return "You do not have a licensed seat for Ansible Lightspeed and your organization is using the paid commercial service. Contact your Red Hat Organization's administrator for more information on how to get a licensed seat.";
-            }
-            case "permission_denied__org_not_ready_because_wca_not_configured": {
-              return "Contact your administrator to configure IBM watsonx Code Assistant model settings for your organization.";
-            }
-            case "permission_denied__user_trial_expired": {
-              return "Your trial to the generative AI model has expired. Refer to your IBM Cloud Account to re-enable access to the IBM watsonx Code Assistant by moving to one of the paid plans.";
-            }
-            case "permission_denied__user_with_no_seat": {
-              return "You don't have access to IBM watsonx Code Assistant. Contact your administrator.";
-            }
-            default: {
-              return "User not authorized to access Ansible Lightspeed.";
-            }
-          }
-        } else {
-          return "User not authorized to access Ansible Lightspeed.";
-        }
+        return ERRORS_UNAUTHORIZED.message || message;
       }
-    } else if (err?.response?.status.toString().startsWith("5")) {
-      return "Ansible Lightspeed encountered an error. Try again after some time.";
-    } else {
-      return `Failed to fetch inline suggestion from Ansible Lightspeed with status code: ${err?.response?.status}. Try again after some time.`;
     }
-  } else if (err.code === AxiosError.ECONNABORTED) {
-    return "Ansible Lightspeed connection timeout. Try again after some time.";
-  } else {
-    return "Failed to fetch inline suggestion from Ansible Lightspeed. Try again after some time.";
+    if (status === 429) {
+      return ERRORS_TOO_MANY_REQUESTS.message || message;
+    }
+    if (status === 500) {
+      return ERRORS_UNKNOWN.message || message;
+    }
   }
+
+  if (err.code === AxiosError.ECONNABORTED) {
+    return ERRORS_CONNECTION_TIMEOUT.message as string;
+  }
+  return ERRORS_UNKNOWN.message as string;
 }
