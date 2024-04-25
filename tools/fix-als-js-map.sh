4f4aaafe299cdcbd50d78d7bd5a729548cc8424f
Tami Takamiya <ttakamiy@redhat.com>
2024-04-25 12:06:57 -0400

    Redesign ALS source map support

--- tools/fix-als-js-map.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find out/server -name "*.js.map" -exec bash -c "sed -E -e 's#(\.\./\.\./\.\.)/(src|test|tools)/#\1/packages/ansible-language-server/\2/#' \$1 > \$1.tmp && mv \$1.tmp \$1" - {} \;
