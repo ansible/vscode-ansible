77826aeb78c9577febae658d2e19261db92e8e2c
Tami Takamiya <ttakamiy@redhat.com>
2024-04-25 09:25:48 -0400

    Simply the script to deal with macos issue

--- tools/fix-als-js-map.sh
+++ tools/fix-als-js-map.sh
@@ -1,5 +1,2 @@
-#!/bin/sh
-SED_COMMAND="sed -E -e \
-'s#\.\./\.\./\.\./(src|test|tools)/#\.\./\.\./\.\./packages/ansible-language-server/\1/#' \
-{} > {}.tmp && mv {}.tmp {}"
-find out/server -name '*.js.map' -print0 | xargs -0 -I {} sh -c "${SED_COMMAND}"
+#!/bin/bash
+find out/server -name "*.js.map" -exec bash -c "sed -E -e 's#(\.\./\.\./\.\.)/(src|test|tools)/#\1/packages/ansible-language-server/\2/#' \$1 > \$1.tmp && mv \$1.tmp \$1" - {} \;
