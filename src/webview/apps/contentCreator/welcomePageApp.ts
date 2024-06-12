c469283c653031962c472f0b6a6fe11bba3bc79a
priyamsahoo <priyamsahoo.26@gmail.com>
2024-06-12 13:34:23 +0530

    update the new welcome page for ansible content creator

--- src/webview/apps/contentCreator/welcomePageApp.ts
+++ src/webview/apps/contentCreator/welcomePageApp.ts
@@ -9,9 +9,12 @@ provideVSCodeDesignSystem().register(allComponents);
 const vscode = acquireVsCodeApi();
 window.addEventListener("load", main);
 
+let systemReadinessDiv: HTMLElement | null;
 let systemCheckDiv: HTMLElement | null;
 let installStatusDiv: HTMLElement | null;
 
+let systemReadinessIcon: HTMLElement;
+let systemReadinessDescription: HTMLElement;
 let ansibleVersionStatusText: HTMLElement;
 let ansibleLocationStatusText: HTMLElement;
 let pythonVersionStatusText: HTMLElement;
@@ -21,10 +24,15 @@ let ansibleDevEnvironmentStatusText: HTMLElement;
 let refreshButton: Button;
 
 function main() {
+  systemReadinessDiv = document.getElementById("system-readiness");
+
   systemCheckDiv = document.getElementById("system-check");
   if (systemCheckDiv) systemCheckDiv.style.visibility = "hidden";
 
   installStatusDiv = document.getElementById("install-status");
+
+  systemReadinessIcon = document.createElement("section");
+  systemReadinessDescription = document.createElement("section");
 
   ansibleVersionStatusText = document.createElement("section");
   ansibleLocationStatusText = document.createElement("section");
@@ -64,56 +72,77 @@ function updateAnsibleCreatorAvailabilityStatus() {
         const ansibleDevEnvironmentVersion =
           systemDetails["ansible-dev-environment version"];
 
-        if (ansibleVersion) {
-          ansibleVersionStatusText.innerHTML = `<p class='found'>ansible version: ${ansibleVersion}</p>`;
+        const systemStatus = !!(
+          ansibleVersion &&
+          pythonVersion &&
+          ansibleCreatorVersion
+        );
+
+        if (systemStatus) {
+          if (systemReadinessDiv)
+            systemReadinessDiv.style.backgroundColor = "#023020";
+          systemReadinessIcon.innerHTML = `<span class="codicon codicon-pass"></span>`;
+          systemReadinessDescription.innerHTML = `<p class="system-description"><b>All the tools are installed</b>.<br>Your environment is ready and you can start creating ansible content.</p>`;
         } else {
-          ansibleVersionStatusText.innerHTML = `<p class='not-found'>ansible version: Not found</p>`;
+          if (systemReadinessDiv)
+            systemReadinessDiv.style.backgroundColor = "#610000";
+          systemReadinessIcon.innerHTML = `<span class="codicon codicon-error"></span>`;
+          systemReadinessDescription.innerHTML = `<p class="system-description"><b>Looks like your environment is missing some tools</b>.<br>Follow the descrption in the 'System requirements' section to set up your environment.</p>`;
+        }
+        systemReadinessDiv?.appendChild(systemReadinessIcon);
+        systemReadinessDiv?.appendChild(systemReadinessDescription);
+
+        if (ansibleVersion) {
+          ansibleVersionStatusText.innerHTML = `<p class='found'><b>Ansible version:</b> ${ansibleVersion}</p>`;
+        } else {
+          ansibleVersionStatusText.innerHTML = `<p class='not-found'><b>Ansible version:</b> Not found</p>`;
         }
         installStatusDiv?.appendChild(ansibleVersionStatusText);
 
         // ansible location text
         if (ansibleLocation) {
-          ansibleLocationStatusText.innerHTML = `<p class='found'>ansible location: ${ansibleLocation}</p>`;
+          ansibleLocationStatusText.innerHTML = `<p class='found'><b>Ansible location:</b> ${ansibleLocation}</p>`;
         } else {
-          ansibleLocationStatusText.innerHTML = `<p class='not-found'>ansible location: Not found</p>`;
+          ansibleLocationStatusText.innerHTML = `<p class='not-found'><b>Ansible location:</b> Not found</p>`;
         }
         installStatusDiv?.appendChild(ansibleLocationStatusText);
 
         // python version text
         if (pythonVersion) {
-          pythonVersionStatusText.innerHTML = `<p class='found'>python version: ${pythonVersion}</p>`;
+          pythonVersionStatusText.innerHTML = `<p class='found'><b>Python version:</b> ${pythonVersion}</p>`;
         } else {
-          pythonVersionStatusText.innerHTML = `<p class='not-found'>python version: Not found</p>`;
+          pythonVersionStatusText.innerHTML = `<p class='not-found'><b>Python version:</b> Not found</p>`;
         }
         installStatusDiv?.appendChild(pythonVersionStatusText);
 
         // python location text
         if (pythonLocation) {
-          pythonLocationStatusText.innerHTML = `<p class='found'>python location: ${pythonLocation}</p>`;
+          pythonLocationStatusText.innerHTML = `<p class='found'><b>Python location:</b> ${pythonLocation}</p>`;
         } else {
-          pythonLocationStatusText.innerHTML = `<p class='not-found'>python location: Not found</p>`;
+          pythonLocationStatusText.innerHTML = `<p class='not-found'><b>Python location:</b> Not found</p>`;
         }
         installStatusDiv?.appendChild(pythonLocationStatusText);
 
+        // ade version text
+        if (ansibleDevEnvironmentVersion) {
+          ansibleDevEnvironmentStatusText.innerHTML = `<p class='found'>[optional] <b>Ansible-dev-environment version:</b> ${ansibleDevEnvironmentVersion}</p>`;
+        } else {
+          ansibleDevEnvironmentStatusText.innerHTML = `<p class='not-found-optional'>[optional] <b>Ansible-dev-environment version:</b> Not found</p>`;
+        }
+        installStatusDiv?.appendChild(ansibleDevEnvironmentStatusText);
+
         // ansible-creator version text
         if (ansibleCreatorVersion) {
-          ansibleCreatorVersionStatusText.innerHTML = `<p class='found'>ansible-creator version: ${ansibleCreatorVersion}</p>`;
+          ansibleCreatorVersionStatusText.innerHTML = `<p class='found'><b>Ansible-creator version:</b> ${ansibleCreatorVersion}</p>`;
         } else {
           ansibleCreatorVersionStatusText.innerHTML = `
-          <p class='not-found'>ansible-creator version: Not found</p>
+          <p class='not-found'><b>Ansible-creator version:</b> Not found</p>
+          <br>
           <p>Before getting started, please <vscode-link href="command:ansible.content-creator.install">install ansible-creator</vscode-link> (via pip) or <br>
           <vscode-link href="command:ansible.python-settings.open">switch to a different python interpreter</vscode-link>  with ansible-creator already installed in it.</p>
           `;
         }
         installStatusDiv?.appendChild(ansibleCreatorVersionStatusText);
-
-        // ade version text
-        if (ansibleDevEnvironmentVersion) {
-          ansibleDevEnvironmentStatusText.innerHTML = `<p class='found'>[optional] ansible-dev-environment version: ${ansibleDevEnvironmentVersion}</p>`;
-        } else {
-          ansibleDevEnvironmentStatusText.innerHTML = `<p class='not-found-optional'>[optional] ansible-dev-environment version: Not found</p>`;
-        }
-        installStatusDiv?.appendChild(ansibleDevEnvironmentStatusText);
       }
       // <p>&#x2717; python version: ${pythonVersion}</p>
       // <p>&#x2717; python location: ${pythonLocation}</p>
