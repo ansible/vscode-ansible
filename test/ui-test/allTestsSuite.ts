2065c87197fd4b751fc0d28b06a644c23cd0937b
Tami Takamiya <ttakamiy@redhat.com>
2024-07-01 12:52:38 -0400

    Lightspeed Authentication UI test on macos (#1410)

--- test/ui-test/allTestsSuite.ts
+++ test/ui-test/allTestsSuite.ts
@@ -1,10 +1,17 @@
 import { extensionUIAssetsTest } from "./extensionUITest";
-import { lightspeedUIAuthTest } from "./lightspeedAuthUiTest";
+import {
+  lightspeedUILoginTest,
+  lightspeedUISignOutTest,
+} from "./lightspeedAuthUiTest";
 import { lightspeedUIAssetsTest } from "./lightspeedUiTest";
 
 describe("VSCode Ansible - UI tests", function () {
   this.timeout(30000);
   extensionUIAssetsTest();
-  lightspeedUIAuthTest();
   lightspeedUIAssetsTest();
+  lightspeedUILoginTest();
+  // Skip this on MacOS due to the functional limitation on menu support
+  if (process.platform !== "darwin") {
+    lightspeedUISignOutTest();
+  }
 });
