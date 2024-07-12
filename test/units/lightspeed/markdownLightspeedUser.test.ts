49a4b592381ba70c60272c5aa1a7af61bae13503
Siddharth Rajaraman <sidrajaraman@gmail.com>
2024-07-12 09:56:42 -0400

    adding name to test

--- test/units/lightspeed/markdownLightspeedUser.test.ts
+++ test/units/lightspeed/markdownLightspeedUser.test.ts
@@ -2,7 +2,7 @@ import assert from "assert";
 import { newGetUserInfoFromMarkdown } from "../../../src/features/lightspeed/markdownLightspeedUser";
 
 describe("Test markdownLightspeedUser", () => {
-  it(() => {
+  it("test newGetUserInfoFromMarkdown", () => {
     const out = newGetUserInfoFromMarkdown();
     assert.equal(out, "hi");
   });
