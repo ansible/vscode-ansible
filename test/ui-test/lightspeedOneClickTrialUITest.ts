81d87b55c136006f234a3814f587e06e1d05b7ac
Gonéri Le Bouder <goneri@lebouder.net>
2024-07-30 16:23:35 -0400

    lightspeed/trial: adjust the pop-up message

--- test/ui-test/lightspeedOneClickTrialUITest.ts
+++ test/ui-test/lightspeedOneClickTrialUITest.ts
@@ -24,7 +24,7 @@ import { expect } from "chai";
 import axios from "axios";
 
 const trialNotificationMessage =
-  "Model settings are not configured for your organization, but your account is eligible to start a 90-day trial.";
+  "Ansible Lightspeed is not configured for your organization, click here to start a 90-day trial.";
 
 export function lightspeedOneClickTrialUITest(): void {
   describe("Test One Click Trial feature", () => {
