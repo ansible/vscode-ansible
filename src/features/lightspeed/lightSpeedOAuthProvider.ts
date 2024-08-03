6b0ca571e88c37e192263733c4e771e32458cd56
Robin Bobbitt <rbobbitt@redhat.com>
2024-08-03 07:44:30 -0400

    Additional logging when proxy block requests to lightspeed (#1490)
    * additional logging when proxy block requests to lightspeed
    
    * add istanbul ignore for error logging

--- src/features/lightspeed/lightSpeedOAuthProvider.ts
+++ src/features/lightspeed/lightSpeedOAuthProvider.ts
@@ -400,6 +400,11 @@ export class LightSpeedAuthenticationProvider
           "[ansible-lightspeed-oauth] error message: ",
           error.message,
         );
+        /* istanbul ignore next */
+        console.error(
+          "[ansible-lightspeed-oauth] error response data: ",
+          error.response?.data,
+        );
         throw new Error("An unexpected error occurred");
       } else {
         console.error("[ansible-lightspeed-oauth] unexpected error: ", error);
