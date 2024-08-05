9b4fa67eadaa110ccb807dcb1d2fd794a89a78b4
Ajinkya <ajinkyaudgirkar@gmail.com>
2024-08-05 20:03:35 +0530

    fix setup script

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -380,9 +380,10 @@ if [[ "${DOCKER_VERSION}" != 'null' ]] && [[ "${SKIP_DOCKER:-}" != '1' ]]; then
         docker run --rm "${IMAGE}" ansible --version)
     EE_ANSIBLE_LINT_VERSION=$(get_version \
         docker run "${IMAGE}" ansible-lint --nocolor --version)
+    log notice "ansible: ${EE_ANSIBLE_VERSION}, ansible-lint: ${EE_ANSIBLE_LINT_VERSION}"
     # Test docker ability to mount current folder with write access, default mount options
-    docker run -v "$PWD:$PWD" --security-opt label=disable ghcr.io/ansible/community-ansible-dev-tools:latest \
-        bash -c "[ -w $PWD ] && echo 'Mounts working' || { echo 'Mounts not working. You might need to either disable or make selinux permissive.'; exit 1; }"
+    docker run -v "$PWD:$PWD" ghcr.io/ansible/community-ansible-dev-tools:latest \
+        bash -c "[ -e $PWD ] && [ -d $PWD ] && echo 'Mounts working' || { echo 'Mounts not working. You might need to either disable or make selinux permissive.'; exit 1; }"
 fi
 
 log notice "Podman checks..."
@@ -442,10 +443,12 @@ if [[ "${PODMAN_VERSION}" != 'null' ]] && [[ "${SKIP_PODMAN:-}" != '1' ]]; then
     log notice "Retrieving ansible-lint version from ee"
     EE_ANSIBLE_LINT_VERSION=$(get_version \
         podman run --rm "${IMAGE}" ansible-lint --nocolor --version)
+    log notice "ansible: ${EE_ANSIBLE_VERSION}, ansible-lint: ${EE_ANSIBLE_LINT_VERSION}"
     log notice "Test podman ability to mount current folder with write access, default mount options"
-    podman run -v "$PWD:$PWD" ghcr.io/ansible/community-ansible-dev-tools:latest \
-        bash -c "[ -w $PWD ]"
-        # && echo 'Mounts working' || { echo 'Mounts not working. You might need to either disable or make selinux permissive.'; exit 1; }"
+    podman run -v "$PWD:$PWD" \
+        ghcr.io/ansible/community-ansible-dev-tools:latest \
+        bash -c "[ -e $PWD ] && [ -d $PWD ] \
+        && echo 'Mounts working' || { echo 'Mounts not working. You might need to either disable or make selinux permissive.'; exit 1; }"
 fi
 
 if [[ -f "/usr/bin/apt-get" ]]; then
