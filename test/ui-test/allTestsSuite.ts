4043fb8e790064e9a4b132a6bf37c1cc97a285e8
Tami Takamiya <ttakamiy@redhat.com>
2024-09-24 13:17:40 -0400

    UI Test Stabilization

--- test/ui-test/allTestsSuite.ts
+++ test/ui-test/allTestsSuite.ts
@@ -6,16 +6,22 @@ import { terminalUITests } from "./terminalUiTest";
 
 describe("VSCode Ansible - UI tests", function () {
   this.timeout(30000);
-  extensionUIAssetsTest();
-  lightspeedUIAssetsTest();
-  terminalUITests();
 
-  // Skip this on MacOS due to the functional limitation on menu support
-  if (process.platform === "darwin") {
-    lightspeedUILoginTest();
-  } else {
-    lightspeedOneClickTrialUITest();
-    // lightspeedUILoginTest();
-    // lightspeedUISignOutTest();
-  }
+  describe("UI Test Suite 1/3", () => {
+    extensionUIAssetsTest();
+    lightspeedUIAssetsTest();
+  });
+  describe("UI Test Suite 2/3", () => {
+    terminalUITests();
+  });
+  describe("UI Test Suite 3/3", () => {
+    // Skip this on MacOS due to the functional limitation on menu support
+    if (process.platform === "darwin") {
+      lightspeedUILoginTest();
+    } else {
+      lightspeedOneClickTrialUITest();
+      // lightspeedUILoginTest();
+      // lightspeedUISignOutTest();
+    }
+  });
 });
