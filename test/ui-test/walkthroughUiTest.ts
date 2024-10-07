33645111f03170d3501261af75deee1acb13fede
Alison Hart <alhart@redhat.com>
2024-10-07 03:59:49 -0400

    Add the Discover Ansible Development Tools Walkthrough (#1563)
    * Add the Discover Ansible Development Tools walkthrough
    
    * Adding Check out other walkthroughs section and command
    
    * Fixing terminology according to branding standards
    
    ---------
    
    Co-authored-by: Ajinkya <ajinkyaudgirkar@gmail.com>

--- test/ui-test/walkthroughUiTest.ts
+++ test/ui-test/walkthroughUiTest.ts
@@ -4,15 +4,21 @@ import { sleep } from "./uiTestHelper";
 
 config.truncateThreshold = 0;
 export function walkthroughUiTest(): void {
+  let workbench: Workbench;
+  let editorView: EditorView;
+
+  before(async () => {
+    workbench = new Workbench();
+    editorView = new EditorView();
+  });
+
+  after(async function () {
+    if (editorView) {
+      await editorView.closeAllEditors();
+    }
+  });
+
   describe("Check for the walkthrough - Create an Ansible environment", async () => {
-    let workbench: Workbench;
-    let editorView: EditorView;
-
-    before(async () => {
-      workbench = new Workbench();
-      editorView = new EditorView();
-    });
-
     it("Open the walkthrough and check the elements", async function () {
       const commandInput = await workbench.openCommandPrompt();
       await workbench.executeCommand("Welcome: Open Walkthrough");
@@ -47,6 +53,46 @@ export function walkthroughUiTest(): void {
         "Create an Ansible playbook",
         "tag in the status bar",
         "Install the Ansible environment package",
+      ]);
+    });
+  });
+
+  describe("Check for the walkthrough - Discover Ansible Development Tools", async () => {
+    it("Open the walkthrough and check the elements", async function () {
+      const commandInput = await workbench.openCommandPrompt();
+      await workbench.executeCommand("Welcome: Open Walkthrough");
+      await commandInput.setText("Discover Ansible Development Tools");
+      await commandInput.confirm();
+
+      await sleep(1000);
+
+      // Select the editor window
+      const welcomeTab = await editorView.getTabByTitle("Welcome");
+      expect(welcomeTab).is.not.undefined;
+
+      // Locate walkthrough title text
+      const titleText = await welcomeTab
+        .findElement(
+          By.xpath("//div[contains(@class, 'getting-started-category') ]"),
+        )
+        .getText();
+      expect(
+        titleText.includes("Discover Ansible Development Tools"),
+        "Discover Ansible Development Tools title not found",
+      ).to.be.true;
+
+      // Locate one of the steps
+      const stepText = await welcomeTab
+        .findElement(
+          By.xpath("//div[contains(@class, 'step-list-container') ]"),
+        )
+        .getText();
+
+      expect(stepText).to.contain.oneOf([
+        "Create",
+        "Test",
+        "Deploy",
+        "Where do I start",
       ]);
     });
   });
