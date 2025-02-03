4cd0f056e84e7eeda51ba17125492aa756e9239d
tanwigeetika1618 <tengverified@gmail.com>
2025-02-04 00:45:45 +0530

    Add more tests to increase coverage

--- test/ui-test/contentCreatorUiTestNoCreatorTest.ts
+++ test/ui-test/contentCreatorUiTestNoCreatorTest.ts
@@ -249,6 +249,34 @@ describe("Test execution-environment generation webview (without creator)", () =
     await checkAndInteractWithEEField(
       eeWebview,
       "customBaseImage-name",
+      "quay.io/new/custom",
+    );
+
+    await checkAndInteractWithEEField(
+      eeWebview,
+      "tag-name",
+      "ansible-ee:new-test",
+    );
+    const buildImageCheckbox = await eeWebview.findWebElement(
+      By.xpath("//vscode-checkbox[@id='buildImage-checkbox']"),
+    );
+    await buildImageCheckbox.click();
+    await sleep(1000);
+
+    await clickButtonAndCheckEnabled(eeWebview, "create-button");
+    await sleep(1000);
+
+    await overwriteCheckbox.click();
+    await clickButtonAndCheckEnabled(eeWebview, "create-button");
+    await sleep(1000);
+
+    await clickButtonAndCheckEnabled(eeWebview, "clear-button");
+    await clickButtonAndCheckEnabled(eeWebview, "clear-logs-button");
+
+    await checkAndInteractWithEEField(eeWebview, "path-url", os.homedir());
+    await checkAndInteractWithEEField(
+      eeWebview,
+      "customBaseImage-name",
       "quay.io/fedora/fedora:41",
     );
     await checkAndInteractWithEEField(
@@ -285,6 +313,28 @@ describe("Test execution-environment generation webview (without creator)", () =
 
     await clickButtonAndCheckEnabled(eeWebview, "clear-button");
     await sleep(1000);
+
+    await checkAndInteractWithEEField(eeWebview, "path-url", os.homedir());
+    await checkAndInteractWithEEField(
+      eeWebview,
+      "tag-name",
+      "ansible-ee:latest",
+    );
+    await clickButtonAndCheckEnabled(eeWebview, "create-button");
+    await sleep(1000);
+    const openScaffoldedFileButton = await eeWebview.findWebElement(
+      By.xpath("//vscode-button[@id='init-open-scaffolded-file']"),
+    );
+    expect(await openScaffoldedFileButton.isDisplayed()).to.be.true;
+    await openScaffoldedFileButton.click();
+    await sleep(1000);
+
+    const editorView = new EditorView();
+    const activeTabs = await editorView.getOpenEditorTitles();
+    expect(activeTabs).to.include("execution.yml");
+    const firstEditorTitle = activeTabs[0];
+    expect(firstEditorTitle).to.equal("execution.yml");
+
     await eeWebview.switchBack();
   });
 
