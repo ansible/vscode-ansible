2469bd00f832b72f01bef83d63e2e17f1eab1b7b
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2025-02-17 12:40:45 +0000

    Use . instead of source for sourcing scripts
    As not all shells supports 'source' command, we should always use
    the '.' command which is more portable.
    
    sh points to dash on Ubuntu and does not have source.
    
    This change also adds one test that enforces use of activationScript
    in order to check its validity.
    
    Fixes: #1830
    Fixes: #1827
    Closes: #1831

--- packages/ansible-language-server/test/utils/runCommand.test.ts
+++ packages/ansible-language-server/test/utils/runCommand.test.ts
@@ -26,6 +26,7 @@ describe("commandRunner", () => {
       stdout: `${pkgJSON["version"]}`,
       stderr: "",
       pythonInterpreterPath: "",
+      activationScript: "",
     },
     {
       args: ["ansible-config", "dump"],
@@ -33,6 +34,7 @@ describe("commandRunner", () => {
       stdout: "ANSIBLE_FORCE_COLOR",
       stderr: "",
       pythonInterpreterPath: "",
+      activationScript: "",
     },
     {
       args: ["ansible", "--version"],
@@ -40,6 +42,7 @@ describe("commandRunner", () => {
       stdout: "configured module search path",
       stderr: "",
       pythonInterpreterPath: "",
+      activationScript: "",
     },
     {
       args: ["ansible-lint", "--version"],
@@ -47,6 +50,7 @@ describe("commandRunner", () => {
       stdout: "using ansible",
       stderr: "",
       pythonInterpreterPath: "",
+      activationScript: "",
     },
     {
       args: ["ansible-playbook", "missing-file"],
@@ -54,6 +58,7 @@ describe("commandRunner", () => {
       stdout: "",
       stderr: "ERROR! the playbook: missing-file could not be found",
       pythonInterpreterPath: "",
+      activationScript: "",
     },
     {
       args: [
@@ -65,48 +70,66 @@ describe("commandRunner", () => {
       stdout: "path-before-python",
       stderr: "",
       pythonInterpreterPath: "path-before-python/bin/python",
+      activationScript: "",
+    },
+    {
+      args: ["echo", "123"],
+      rc: 0,
+      stdout: "123",
+      stderr: "",
+      pythonInterpreterPath: "path-before-python/bin/python",
+      activationScript: `${process.env.VIRTUAL_ENV}/bin/activate`,
     },
   ];
 
-  tests.forEach(({ args, rc, stdout, stderr, pythonInterpreterPath }) => {
-    it(`call ${args.join(" ")}`, async function () {
-      this.timeout(10000);
+  tests.forEach(
+    ({ args, rc, stdout, stderr, pythonInterpreterPath, activationScript }) => {
+      it(`call ${args.join(" ")}`, async function () {
+        this.timeout(10000);
 
-      // try to enforce ansible to output ANSI in order to check if we are
-      // still able to disable it at runtime in order to keep output parseable.
-      process.env.ANSIBLE_FORCE_COLOR = "1";
+        // try to enforce ansible to output ANSI in order to check if we are
+        // still able to disable it at runtime in order to keep output parseable.
+        process.env.ANSIBLE_FORCE_COLOR = "1";
 
-      process.argv.push("--node-ipc");
-      const connection = createConnection();
-      const workspaceManager = new WorkspaceManager(connection);
-      const textDoc = getDoc("yaml/ancestryBuilder.yml");
-      const context = workspaceManager.getContext(textDoc.uri);
-      if (context) {
-        const settings = await context.documentSettings.get(textDoc.uri);
-        if (pythonInterpreterPath) {
-          settings.python.interpreterPath = pythonInterpreterPath;
-        }
+        process.argv.push("--node-ipc");
+        const connection = createConnection();
+        const workspaceManager = new WorkspaceManager(connection);
+        const textDoc = getDoc("yaml/ancestryBuilder.yml");
+        const context = workspaceManager.getContext(textDoc.uri);
+        if (context) {
+          const settings = await context.documentSettings.get(textDoc.uri);
+          if (pythonInterpreterPath) {
+            settings.python.interpreterPath = pythonInterpreterPath;
+          }
+          if (activationScript) {
+            settings.python.activationScript = activationScript;
+          }
 
-        const commandRunner = new CommandRunner(connection, context, settings);
-        try {
-          const proc = await commandRunner.runCommand(
-            args[0],
-            args.slice(1).join(" "),
+          const commandRunner = new CommandRunner(
+            connection,
+            context,
+            settings,
           );
-          expect(proc.stdout, proc.stderr).contains(stdout);
-          expect(proc.stderr, proc.stdout).contains(stderr);
-        } catch (e) {
-          if (e instanceof AssertionError) {
-            throw e;
-          }
-          if (e instanceof Error) {
-            const err = e as ExecException;
-            expect(err.code).equals(rc);
-            expect(err.stdout).contains(stdout);
-            expect(err.stderr).contains(stderr);
+          try {
+            const proc = await commandRunner.runCommand(
+              args[0],
+              args.slice(1).join(" "),
+            );
+            expect(proc.stdout, proc.stderr).contains(stdout);
+            expect(proc.stderr, proc.stdout).contains(stderr);
+          } catch (e) {
+            if (e instanceof AssertionError) {
+              throw e;
+            }
+            if (e instanceof Error) {
+              const err = e as ExecException;
+              expect(err.code).equals(rc);
+              expect(err.stdout).contains(stdout);
+              expect(err.stderr).contains(stderr);
+            }
           }
         }
-      }
-    });
-  });
+      });
+    },
+  );
 });
--- src/features/ansibleTox/runner.ts
+++ src/features/ansibleTox/runner.ts
@@ -28,7 +28,7 @@ export async function getToxEnvs(
   )) as string;
 
   if (activationScript) {
-    command = `sh -c 'source ${activationScript} && ${command}'`;
+    command = `sh -c '. ${activationScript} && ${command}'`;
   }
   if (interpreterPath && interpreterPath !== "") {
     const virtualEnv = path.resolve(interpreterPath, "../..");
