1f05ab64530b53c6ad239ad7e523179915935267
Gonéri Le Bouder <goneri@lebouder.net>
2025-02-18 15:19:36 -0500

    roleGen: properly reset the outline when they are changed manually
    When the user goes back to the first page, we need to ignore the `outline` used
    previously or we will mess up the next role generation request.
    
    - reset the request if the outline get changed
    - don't reset the outline if the user get back on page one
      - the outline and request will be reset only if the user changes the prompt
    - keep the prompt if the WCA answer has not prompt set
    - finally, if `selectionStart()` returns `undefined`, we default on `0`

--- webviews/lightspeed/role-generation/src/components/OutlineReview.vue
+++ webviews/lightspeed/role-generation/src/components/OutlineReview.vue
@@ -6,21 +6,21 @@ const emit = defineEmits<{ outlineUpdate: [outline: string] }>();
 
 function outlineWithLineNumber() {
   let textField = document.querySelector("#outline-field") as HTMLTextAreaElement;
-  const originalPosition = textField?.selectionStart;
 
+  const originalPosition = textField?.selectionStart || 0;
   textField.value = textField.value.split("\n").map((l) => l.replace(/\d+\.\s/, '')).map((l, idx) => { return `${idx + 1}. ${l}` }).join("\n");
+
   const newPosition = textField.value[originalPosition - 1] === '\n' ? originalPosition + 3 : originalPosition;
   textField.setSelectionRange(newPosition, newPosition)
   emit("outlineUpdate", textField.value);
-
 }
 </script>
 
 <template>
   <div>
     <h4>Review the suggested steps for your role and modify as needed.</h4>
-    <textarea id="outline-field" :rows="outline.split('\n').length + 2" :cols="outline.split('\n')[0].length + 5"
-      :value="outline.toString()" @input="outlineWithLineNumber" />
+    <textarea id="outline-field" :rows="outline.split('\n').length + 2" :cols="70" :value="outline.toString()"
+      @input="outlineWithLineNumber" />
   </div>
 </template>
 
