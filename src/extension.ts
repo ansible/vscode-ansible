367eac857b893ef17c6e02f0899c9ae608735806
Michael Abashian <mabashian@users.noreply.github.com>
2025-05-16 08:40:30 -0400

    Fix bug where role explanation always shows up in context menu (#2021)

--- src/extension.ts
+++ src/extension.ts
@@ -310,6 +310,7 @@ export async function activate(context: ExtensionContext): Promise<void> {
           lightSpeedManager,
           pythonInterpreterManager,
         );
+        await updateDocumentInRoleContext();
         if (!editor) {
           await ignorePendingSuggestion();
         }
@@ -1062,4 +1063,16 @@ async function lightspeedLogin(
       `Welcome back ${authenticatedUser.displayNameWithUserType}`,
     );
   }
+}
+
+async function updateDocumentInRoleContext() {
+  const document = vscode.window.activeTextEditor?.document;
+  const isInRole = document
+    ? await isDocumentInRole(document).catch(() => false)
+    : false;
+  vscode.commands.executeCommand(
+    "setContext",
+    "redhat.ansible.isDocumentInRole",
+    isInRole,
+  );
 }
