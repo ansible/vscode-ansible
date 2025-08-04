e953860e754bc839c3166761b9c341f43324baa6
alisonlhart <alhart@redhat.com>
2025-08-04 12:04:15 -0400

    Add check for WSL runner before running e2e test

--- test/e2e/diagnostics/testYamlWithoutEE.test.ts
+++ test/e2e/diagnostics/testYamlWithoutEE.test.ts
@@ -15,61 +15,62 @@ export function testDiagnosticsYAMLWithoutEE(): void {
     before(async function () {
       await vscode.commands.executeCommand("workbench.action.closeAllEditors");
     });
+    if (process.env.IS_WSL !== "1") {
+      describe("YAML diagnostics in the presence of ansible-lint", function () {
+        it("should provide diagnostics with YAML validation (with ansible-lint)", async function () {
+          await activate(docUri1);
+          await waitForDiagnosisCompletion(); // Wait for the diagnostics to compute on this file
 
-    describe("YAML diagnostics in the presence of ansible-lint", function () {
-      it("should provide diagnostics with YAML validation (with ansible-lint)", async function () {
-        await activate(docUri1);
-        await waitForDiagnosisCompletion(); // Wait for the diagnostics to compute on this file
-
-        await testDiagnostics(docUri1, [
-          {
-            severity: 1,
-            message: "Failed to load YAML file",
-            range: new vscode.Range(
-              new vscode.Position(6, 21),
-              new vscode.Position(6, integer.MAX_VALUE),
-            ),
-            source: "ansible-lint",
-          },
-          {
-            severity: 0,
-            message: "Nested mappings are not allowed in compact mappings",
-            range: new vscode.Range(
-              new vscode.Position(6, 13),
-              new vscode.Position(6, 14),
-            ),
-            source: "Ansible [YAML]",
-          },
-          {
-            severity: 0,
-            message: "Unexpected scalar at node end",
-            range: new vscode.Range(
-              new vscode.Position(7, 0),
-              new vscode.Position(7, 6),
-            ),
-            source: "Ansible [YAML]",
-          },
-          {
-            severity: 0,
-            message: "Unexpected map-value-ind token in YAML stream",
-            range: new vscode.Range(
-              new vscode.Position(7, 6),
-              new vscode.Position(7, 7),
-            ),
-            source: "Ansible [YAML]",
-          },
-          {
-            severity: 0,
-            message: "Unexpected scalar token in YAML stream",
-            range: new vscode.Range(
-              new vscode.Position(7, 8),
-              new vscode.Position(7, 12),
-            ),
-            source: "Ansible [YAML]",
-          },
-        ]);
+          await testDiagnostics(docUri1, [
+            {
+              severity: 1,
+              message: "Failed to load YAML file",
+              range: new vscode.Range(
+                new vscode.Position(6, 21),
+                new vscode.Position(6, integer.MAX_VALUE),
+              ),
+              source: "ansible-lint",
+            },
+            {
+              severity: 0,
+              message: "Nested mappings are not allowed in compact mappings",
+              range: new vscode.Range(
+                new vscode.Position(6, 13),
+                new vscode.Position(6, 14),
+              ),
+              source: "Ansible [YAML]",
+            },
+            {
+              severity: 0,
+              message: "Unexpected scalar at node end",
+              range: new vscode.Range(
+                new vscode.Position(7, 0),
+                new vscode.Position(7, 6),
+              ),
+              source: "Ansible [YAML]",
+            },
+            {
+              severity: 0,
+              message: "Unexpected map-value-ind token in YAML stream",
+              range: new vscode.Range(
+                new vscode.Position(7, 6),
+                new vscode.Position(7, 7),
+              ),
+              source: "Ansible [YAML]",
+            },
+            {
+              severity: 0,
+              message: "Unexpected scalar token in YAML stream",
+              range: new vscode.Range(
+                new vscode.Position(7, 8),
+                new vscode.Position(7, 12),
+              ),
+              source: "Ansible [YAML]",
+            },
+          ]);
+        });
       });
-    });
+    }
 
     describe("YAML diagnostics in the absence of ansible-lint", function () {
       before(async function () {
