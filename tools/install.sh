3ef166ff5544a72389acd1de2e1c7e8fe432ffcc
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2025-12-02 10:48:01 +0000

    chore: include ansible-dev-tools into uv deps

--- tools/install.sh
+++ tools/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-creator_resources_path="$(python -c "from pathlib import Path; import ansible_creator; print(Path(ansible_creator.__file__).parent)")/resources"
+creator_resources_path="$(uv run python -c "from pathlib import Path; import ansible_creator; print(Path(ansible_creator.__file__).parent)")/resources"
 mkdir -p resources/contentCreator/createDevcontainer
 mkdir -p resources/contentCreator/createDevfile
 ln -fs "$creator_resources_path/common/devfile/devfile.yaml.j2" "resources/contentCreator/createDevfile/devfile-template.txt"
