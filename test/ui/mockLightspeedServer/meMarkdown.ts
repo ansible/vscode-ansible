a1414fafe5d9729ffef9fc06a2e31dadb356fa81
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2026-02-06 13:57:34 +0000

    chore: remove 'ui' tests (extest)
    Related: AAP-65141

--- test/ui/mockLightspeedServer/meMarkdown.ts
+++
@@ -1,9 +0,0 @@
-import { options } from "./server";
-export function meMarkdown() {
-  let content =
-    "Logged in as: ONE_CLICK_USER (unlicensed)\n\n User Type: Unlicensed";
-  if (options.meUppercase) {
-    content = content.toUpperCase();
-  }
-  return { content };
-}
